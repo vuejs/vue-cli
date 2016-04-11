d917298a4c7bf099842fe3e16a2af6afc208eb57
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-04-11 19:07:05 +0200

    Add validation for package name in vue init	 (#73)
    * npm install --save validate-npm-package-name
    
    * Add validation for package name in vue init
    
    * Add test case for validation

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -7,10 +7,26 @@ const crypto = require('crypto')
 const render = require('consolidate').handlebars.render
 const inquirer = require('inquirer')
 const async = require('async')
+const extend = Object.assign || require('util')._extend
 const generate = require('../../lib/generate')
 
 const MOCK_TEMPLATE_REPO_PATH = './test/e2e/mock-template-repo'
 const MOCK_TEMPLATE_BUILD_PATH = path.resolve('./test/e2e/mock-template-build')
+
+function monkeyPatchInquirer(answers) {
+  // monkey patch inquirer
+  inquirer.prompt = (questions, cb) => {
+    const key = questions[0].name
+    const _answers = {}
+    const validate = questions[0].validate
+    const valid = validate(answers[key])
+    if (valid !== true) {
+      throw new Error(valid)
+    }
+    _answers[key] = answers[key]
+    cb(_answers)
+  };
+}
 
 describe('vue-cli', () => {
   const answers = {
@@ -24,15 +40,8 @@ describe('vue-cli', () => {
     pick: 'no'
   }
 
-  // monkey patch inquirer
-  inquirer.prompt = (questions, cb) => {
-    const key = questions[0].name
-    const _answers = {}
-    _answers[key] = answers[key]
-    cb(_answers)
-  }
-
   it('template generation', done => {
+    monkeyPatchInquirer(answers)
     generate('test', MOCK_TEMPLATE_REPO_PATH, MOCK_TEMPLATE_BUILD_PATH, err => {
       if (err) done(err)
 
@@ -56,6 +65,7 @@ describe('vue-cli', () => {
   })
 
   it('avoid rendering files that do not have mustaches', done => {
+    monkeyPatchInquirer(answers)
     const binFilePath = `${MOCK_TEMPLATE_REPO_PATH}/template/bin.file`
     const wstream = fs.createWriteStream(binFilePath)
     wstream.write(crypto.randomBytes(100))
@@ -77,6 +87,16 @@ describe('vue-cli', () => {
 
         done()
       })
+    })
+  })
+
+  it('validate input value', done => {
+    // deep copy
+    var invalidName = extend({}, answers, {name: 'INVALID-NAME'})
+    monkeyPatchInquirer(invalidName)
+    generate('INVALID-NAME', MOCK_TEMPLATE_REPO_PATH, MOCK_TEMPLATE_BUILD_PATH, err => {
+      expect(err).to.be.an('error');
+      done()
     })
   })
 })
