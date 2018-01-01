389b8e622132c69f051d30bd95691042b2146510
Evan You <yyx990803@gmail.com>
2017-12-31 20:18:58 -0500

    tweak lint output

--- packages/@vue/cli-plugin-eslint/index.js
+++ packages/@vue/cli-plugin-eslint/index.js
@@ -1,16 +1,27 @@
 module.exports = (api, { lintOnSave }) => {
+  if (lintOnSave) {
+    api.chainWebpack(webpackConfig => {
+      // TODO eslint-loader
+    })
+  }
+
   api.registerCommand('lint', {
     descriptions: 'lint source files',
     usage: 'vue-cli-service lint [options] [...files]',
     options: {
-      '--fix': 'auto fix lint errors in-place (default: true)',
-      '--format': 'specify formatter (default: codeframe)'
+      '--format': 'specify formatter (default: codeframe)',
+      '--no-fix': 'do not fix errors'
     },
     details: 'For more options, see https://eslint.org/docs/user-guide/command-line-interface#options'
   }, args => {
     const { CLIEngine } = require('eslint')
+    const { done } = require('@vue/cli-shared-utils')
 
     const files = args._.length ? args._ : ['src', 'test']
+    if (args['no-fix']) {
+      args.fix = false
+      delete args['no-fix']
+    }
     const config = Object.assign({
       cwd: api.resolve('.'),
       fix: true,
@@ -31,14 +42,13 @@ module.exports = (api, { lintOnSave }) => {
       CLIEngine.outputFixes(report)
     }
 
-    console.log(formatter(report.results))
+    if (!report.errorCount && !report.warningCount) {
+      const hasFixed = report.results.some(f => f.output)
+      done(hasFixed ? `All lint errors auto-fixed.` : `No lint errors found!`)
+    } else {
+      console.log(formatter(report.results))
+    }
   })
-
-  if (lintOnSave) {
-    api.chainWebpack(webpackConfig => {
-      // TODO eslint-loader
-    })
-  }
 }
 
 function normalizeConfig (args) {
