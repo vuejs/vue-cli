f384ac20517be51bd9f5baf5b9936c47347efb41
Evan You <yyx990803@gmail.com>
2018-01-02 23:59:50 -0500

    sync open-in-editor url w/ devtool

--- packages/@vue/cli-overlay/middleware/index.js
+++ packages/@vue/cli-overlay/middleware/index.js
@@ -2,7 +2,7 @@ const launchEditor = require('./launchEditor')
 
 module.exports = () => {
   return function launchEditorMiddleware (req, res, next) {
-    if (req.url.startsWith('/open-in-editor')) {
+    if (req.url.startsWith('/_open')) {
       launchEditor(req.query.fileName, req.query.lineNumber)
       res.end()
     } else {
