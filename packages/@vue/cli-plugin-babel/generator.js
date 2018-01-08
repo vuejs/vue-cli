15be241628cf43c684dc7ea571035887cb7a6b98
Evan You <yyx990803@gmail.com>
2018-01-07 21:46:58 -0500

    sync deps

--- packages/@vue/cli-plugin-babel/generator.js
+++ packages/@vue/cli-plugin-babel/generator.js
@@ -1,7 +1,7 @@
 module.exports = api => {
   api.extendPackage({
     devDependencies: {
-      '@vue/babel-preset-app': '^0.1.0'
+      '@vue/babel-preset-app': '^3.0.0-alpha.1'
     },
     babel: {
       presets: ['@vue/app']
