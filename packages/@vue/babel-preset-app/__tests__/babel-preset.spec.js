91b20b7754e565cc79d029251568b7a3499e9954
Evan You <yyx990803@gmail.com>
2018-01-11 14:54:49 -0500

    tests for babel preset

---
+++ packages/@vue/babel-preset-app/__tests__/babel-preset.spec.js
@@ -0,0 +1,69 @@
+const babel = require('@babel/core')
+const preset = require('../index')
+const defaultOptions = {
+  babelrc: false,
+  presets: [preset]
+}
+
+test('polyfill detection', () => {
+  let { code } = babel.transformSync(`
+    const a = Promise.resolve()
+  `.trim(), {
+    babelrc: false,
+    presets: [[preset, {
+      targets: { node: 'current' }
+    }]]
+  })
+  expect(code).not.toMatch(`import "core-js/modules/es6.promise"`)
+
+  ;({ code } = babel.transformSync(`
+    const a = Promise.resolve()
+  `.trim(), {
+    babelrc: false,
+    presets: [[preset, {
+      targets: { ie: 9 }
+    }]]
+  }))
+  expect(code).toMatch(`import "core-js/modules/es6.promise"`)
+})
+
+test('object spread', () => {
+  const { code } = babel.transformSync(`
+    const a = { ...b }
+  `.trim(), defaultOptions)
+  expect(code).toMatch(`import "core-js/modules/es6.object.assign"`)
+  expect(code).toMatch(`var a = Object.assign({}, b)`)
+})
+
+test('dynamic import', () => {
+  expect(() => {
+    babel.transformSync(`const Foo = () => import('./Foo.vue')`, defaultOptions)
+  }).not.toThrow()
+})
+
+test('async/await', () => {
+  const { code } = babel.transformSync(`
+    async function hello () {
+      await Promise.resolve()
+    }
+    hello()
+  `.trim(), defaultOptions)
+  expect(code).toMatch(`import "core-js/modules/es6.promise"`)
+  // should use regenerator runtime
+  expect(code).toMatch(`import "regenerator-runtime/runtime"`)
+  // should use required helper instead of inline
+  expect(code).toMatch(`@babel/runtime/helpers/asyncToGenerator`)
+})
+
+test('jsx', () => {
+  const { code } = babel.transformSync(`
+    export default {
+      render () {
+        return <div>bar</div>
+      }
+    }
+  `.trim(), defaultOptions)
+  console.log(code)
+  expect(code).toMatch(`var h = arguments[0]`)
+  expect(code).toMatch(`return h("div", null, ["bar"])`)
+})
