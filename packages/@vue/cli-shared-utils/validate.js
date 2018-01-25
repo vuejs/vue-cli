5fef42cbd6faffd1f7215369d05300490f384bcf
Evan You <yyx990803@gmail.com>
2018-01-24 22:54:12 -0500

    feat: better validation error message

--- packages/@vue/cli-shared-utils/validate.js
+++ packages/@vue/cli-shared-utils/validate.js
@@ -1,4 +1,5 @@
 const joi = require('joi')
+const { error } = require('./logger')
 
 // proxy to joi for option validation
 exports.createSchema = fn => fn(joi)
@@ -6,7 +7,8 @@ exports.createSchema = fn => fn(joi)
 exports.validate = (obj, schema, options = {}) => {
   joi.validate(obj, schema, options, err => {
     if (err) {
-      throw err
+      error(`vue-cli options validation failed:\n` + err.message)
+      process.exit(1)
     }
   })
 }
