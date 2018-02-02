6661ac24ac82b6fce4e753123a21efa083d822ca
Evan You <yyx990803@gmail.com>
2018-02-02 01:12:19 -0500

    fix: fix --force flag

--- packages/@vue/cli/bin/vue.js
+++ packages/@vue/cli/bin/vue.js
@@ -127,7 +127,9 @@ function cleanArgs (cmd) {
   const args = {}
   cmd.options.forEach(o => {
     const key = o.long.replace(/^--/, '')
-    if (typeof cmd[key] === 'string') {
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function') {
       args[key] = cmd[key]
     }
   })
