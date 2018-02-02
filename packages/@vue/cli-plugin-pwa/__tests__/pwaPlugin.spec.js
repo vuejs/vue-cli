fa25cc04182f460d2d97e00073c07f36b1981222
Evan You <yyx990803@gmail.com>
2018-02-02 16:49:06 -0500

    test: bump pwaPlugin test wait for CI

--- packages/@vue/cli-plugin-pwa/__tests__/pwaPlugin.spec.js
+++ packages/@vue/cli-plugin-pwa/__tests__/pwaPlugin.spec.js
@@ -58,7 +58,7 @@ test('pwa', async () => {
   const launched = await launchPuppeteer(`http://localhost:${port}/`)
   browser = launched.browser
 
-  await new Promise(r => setTimeout(r, 500))
+  await new Promise(r => setTimeout(r, process.env.CI ? 1000 : 300))
   const logs = launched.logs
   expect(logs.some(msg => msg.match(/Content has been cached for offline use/))).toBe(true)
   expect(logs.some(msg => msg.match(/App is being served from cache by a service worker/))).toBe(true)
