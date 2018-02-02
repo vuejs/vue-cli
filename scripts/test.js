4f095f0791768076a161379235229e49fa7f4259
Evan You <yyx990803@gmail.com>
2018-02-02 00:54:27 -0500

    workflow: fix test script args

--- scripts/test.js
+++ scripts/test.js
@@ -1,20 +1,26 @@
 const execa = require('execa')
 const minimist = require('minimist')
 
-const args = minimist(process.argv.slice(2))
+const rawArgs = process.argv.slice(2)
+const args = minimist(rawArgs)
 
-let regex = args._[0]
-if (args.p || args.package) {
+let regex
+if (args.p) {
   const packages = (args.p || args.package).split(',').join('|')
   regex = `.*@vue/(${packages}|cli-plugin-(${packages}))/.*\\.spec\\.js$`
+  const i = rawArgs.findIndex('-p')
+  rawArgs.splice(i, 2)
 }
 
 ;(async () => {
-  await execa('jest', [
+  const jestArgs = [
     '--env', 'node',
     '--runInBand',
+    ...rawArgs,
     ...(regex ? [regex] : [])
-  ], {
+  ]
+  console.log(`running jest with args: ${jestArgs.join(' ')}`)
+  await execa('jest', jestArgs, {
     stdio: 'inherit'
   })
 })().catch(err => {
