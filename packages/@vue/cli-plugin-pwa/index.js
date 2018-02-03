83e249ba7aa3eccf7779ffc90f33115f0386873c
Jeff Posnick <jeffy@google.com>
2018-02-02 16:52:31 -0800

    feat: Use the Workbox webpack plugin in pwa template

--- packages/@vue/cli-plugin-pwa/index.js
+++ packages/@vue/cli-plugin-pwa/index.js
@@ -2,25 +2,39 @@ module.exports = (api, options) => {
   api.chainWebpack(webpackConfig => {
     const name = api.service.pkg.name
 
+    const userOptions = options.pwa || {}
+
     // the pwa plugin hooks on to html-webpack-plugin
     // and injects icons, manifest links & other PWA related tags into <head>
     webpackConfig
       .plugin('pwa')
         .use(require('./lib/HtmlPwaPlugin'), [Object.assign({
           name
-        }, options.pwa)])
+        }, userOptions)])
 
     // generate /service-worker.js in production mode
     if (process.env.NODE_ENV === 'production') {
-      webpackConfig
-        .plugin('sw-precache')
-          .use(require('sw-precache-webpack-plugin'), [{
-            cacheId: name,
-            filename: 'service-worker.js',
-            staticFileGlobs: [`${options.outputDir}/**/*.{js,html,css}`],
-            minify: true,
-            stripPrefix: `${options.outputDir}/`
-          }])
+      // Default to GenerateSW mode, though InjectManifest also might be used.
+      const workboxPluginMode = userOptions.workboxPluginMode || 'GenerateSW'
+      const workboxWebpackModule = require('workbox-webpack-plugin')
+      if (workboxPluginMode in workboxWebpackModule) {
+        const workBoxConfig = Object.assign({
+          cacheId: name,
+          exclude: [
+            new RegExp('\.map$'),
+            new RegExp('img/icons/'),
+            new RegExp('favicon\.ico$'),
+            new RegExp('manifest\.json$')
+          ]
+        }, userOptions.workboxOptions)
+
+        webpackConfig
+          .plugin('workbox')
+          .use(workboxWebpackModule[workboxPluginMode], [workBoxConfig])
+      } else {
+        throw new Error(`${workboxPluginMode} is not a supported Workbox webpack plugin mode. ` +
+          `Valid modes are: ${Object.keys(workboxWebpackModule).join(', ')}`)
+      }
     }
   })
 
