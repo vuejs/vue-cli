691cfa2b88a406dcc2168686f4ad726cffba413b
Evan You <yyx990803@gmail.com>
2018-02-04 10:07:08 -0500

    fix: shim global for node modules
    ref #774

--- packages/@vue/cli-service/lib/config/base.js
+++ packages/@vue/cli-service/lib/config/base.js
@@ -92,8 +92,8 @@ module.exports = (api, options) => {
         // prevent webpack from injecting useless setImmediate polyfill because Vue
         // source contains it (although only uses it if it's native).
         setImmediate: false,
+        // process is injected via DefinePlugin
         process: false,
-        global: false,
         // prevent webpack from injecting mocks to Node native modules
         // that does not make sense for the client
         dgram: 'empty',
