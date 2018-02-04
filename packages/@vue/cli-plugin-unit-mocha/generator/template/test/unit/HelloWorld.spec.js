c24dba5475f4346fa8d570a4b6caa658994ba051
Kévin Dunglas <dunglas@gmail.com>
2018-02-04 19:31:26 +0100

    Mocha template: fix the name of the tested file

--- packages/@vue/cli-plugin-unit-mocha/generator/template/test/unit/HelloWorld.spec.js
+++ packages/@vue/cli-plugin-unit-mocha/generator/template/test/unit/HelloWorld.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { shallow } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('Hello.vue', () => {
+describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallow(HelloWorld, {
