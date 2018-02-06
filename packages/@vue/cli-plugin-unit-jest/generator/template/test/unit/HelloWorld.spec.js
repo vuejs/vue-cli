1902d9c41c3f17a043ebb9d7f2513d0b698edd5a
Kévin Dunglas <dunglas@gmail.com>
2018-02-06 19:40:53 +0100

    chore: fix the name of the tested file  (#775)
    * Jest template: fix the name of the tested file (Hello.vue instead of HelloWorld.vue)
    
    * Jest template: fix the name of the tested file

--- packages/@vue/cli-plugin-unit-jest/generator/template/test/unit/HelloWorld.spec.js
+++ packages/@vue/cli-plugin-unit-jest/generator/template/test/unit/HelloWorld.spec.js
@@ -1,7 +1,7 @@
 import { shallow } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('Hello.vue', () => {
+describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallow(HelloWorld, {
