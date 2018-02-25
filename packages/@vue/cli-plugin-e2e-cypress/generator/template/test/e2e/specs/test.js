46358ebbab816c438327f384ea4153e9886922ee
Chris Fritz <chrisvfritz@gmail.com>
2018-02-24 19:54:13 -0500

    fix(cypress): upgrade cypress and properly set base url (#879)

--- packages/@vue/cli-plugin-e2e-cypress/generator/template/test/e2e/specs/test.js
+++ packages/@vue/cli-plugin-e2e-cypress/generator/template/test/e2e/specs/test.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
-    cy.visit(Cypress.env('VUE_DEV_SERVER_URL'))
+    cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js <%- hasTS ? '+ TypeScript ' : '' %>App')
   })
 })
