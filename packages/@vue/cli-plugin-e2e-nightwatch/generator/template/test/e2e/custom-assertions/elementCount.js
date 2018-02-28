f4d18417232bccab46cf87658a22786c1ff9ba63
Evan You <yyx990803@gmail.com>
2018-02-28 17:37:19 -0500

    fix: nightwatch helper compat with airbnb linter
    close #870

--- packages/@vue/cli-plugin-e2e-nightwatch/generator/template/test/e2e/custom-assertions/elementCount.js
+++ packages/@vue/cli-plugin-e2e-nightwatch/generator/template/test/e2e/custom-assertions/elementCount.js
@@ -7,12 +7,13 @@
 // For more information on custom assertions see:
 // http://nightwatchjs.org/guide#writing-custom-assertions
 
-exports.assertion = function (selector, count) {
+exports.assertion = function elementCount (selector, count) {
   this.message = `Testing if element <${selector}> has count: ${count}`
   this.expected = count
   this.pass = val => val === count
   this.value = res => res.value
-  this.command = cb => this.api.execute(function (selector) {
-    return document.querySelectorAll(selector).length
-  }, [selector], cb)
+  function evaluator (_selector) {
+    return document.querySelectorAll(_selector).length
+  }
+  this.command = cb => this.api.execute(evaluator, [selector], cb)
 }
