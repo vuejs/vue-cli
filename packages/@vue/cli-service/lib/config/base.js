a2ac6be36b3d5ade56e3362a4cb847a73e384e29
Evan You <yyx990803@gmail.com>
2018-03-03 19:14:55 -0500

    fix: mock process for 3rd party libs (close #934)

--- packages/@vue/cli-service/lib/config/base.js
+++ packages/@vue/cli-service/lib/config/base.js
@@ -92,8 +92,9 @@ module.exports = (api, options) => {
         // prevent webpack from injecting useless setImmediate polyfill because Vue
         // source contains it (although only uses it if it's native).
         setImmediate: false,
-        // process is injected via DefinePlugin
-        process: false,
+        // process is injected via DefinePlugin, although some 3rd party
+        // libraries may require a mock to work properly (#934)
+        process: 'mock',
         // prevent webpack from injecting mocks to Node native modules
         // that does not make sense for the client
         dgram: 'empty',
