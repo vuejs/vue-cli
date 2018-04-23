445f79386eb56a9546bc3be665969ac6c4f3283d
Guillaume Chau <alphadelta.fg@gmail.com>
2018-04-23 19:35:48 +0200

    docs(ui): Plugin action example

--- packages/@vue/cli-ui-addon-webpack/src/components/TestView.vue
+++ packages/@vue/cli-ui-addon-webpack/src/components/TestView.vue
@@ -2,8 +2,33 @@
   <div class="test-view">
     <h1>I'm a custom view</h1>
     <p>A vue-cli plugin created me! I am a dynamically loaded component paired with a custom route.</p>
+    <div>
+      <VueButton @click="testPluginAction()">Test plugin action</VueButton>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  created () {
+    this.$onPluginActionCalled(action => {
+      console.log('called', action)
+    })
+    this.$onPluginActionResolved(action => {
+      console.log('resolved', action)
+    })
+  },
+
+  methods: {
+    testPluginAction () {
+      this.$callPluginAction('test-action', {
+        meow: 'meow'
+      })
+    }
+  }
+}
+</script>
+
 
 <style lang="stylus" scoped>
 .test-view
