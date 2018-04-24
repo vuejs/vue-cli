7fd167df667782f578ceedd50dff33787aea687f
troy351 <914053923@qq.com>
2018-04-24 15:33:06 +0800

    declare global jsx namespace

--- packages/@vue/cli-plugin-typescript/generator/template/src/shims.d.ts
+++ packages/@vue/cli-plugin-typescript/generator/template/src/shims.d.ts
@@ -1,4 +1,15 @@
+import Vue, { VNode } from 'vue'
+
 declare module '*.vue' {
-  import Vue from 'vue';
-  export default Vue;
+  export default Vue
+}
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
 }
