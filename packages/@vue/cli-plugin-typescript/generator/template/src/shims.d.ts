e7f777bbfb6dc898fa29b689e043b4071f231e70
troy351 <914053923@qq.com>
2018-04-24 16:50:09 +0800

    add tslint disable to prevent log error

--- packages/@vue/cli-plugin-typescript/generator/template/src/shims.d.ts
+++ packages/@vue/cli-plugin-typescript/generator/template/src/shims.d.ts
@@ -6,7 +6,9 @@ declare module '*.vue' {
 
 declare global {
   namespace JSX {
+    // tslint:disable no-empty-interface
     interface Element extends VNode {}
+    // tslint:disable no-empty-interface
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
       [elem: string]: any
