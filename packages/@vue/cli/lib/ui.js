8ebdb05165eeba5c203e392c9e9bd36a018d4c10
Guillaume Chau <alphadelta.fg@gmail.com>
2018-04-28 01:22:15 +0200

    fix(ui): vue ui command moved from cli-service to cli

--- packages/@vue/cli/lib/ui.js
+++ packages/@vue/cli/lib/ui.js
@@ -1,28 +1,51 @@
-const { log, error } = require('@vue/cli-shared-utils')
+const { log, error, openBrowser } = require('@vue/cli-shared-utils')
 const portfinder = require('portfinder')
 const execa = require('execa')
 
 async function ui (options = {}, context = process.cwd()) {
-  log(`🚀  Starting GUI...`)
-
   let port = options.port
   if (!port) {
     port = await portfinder.getPortPromise()
   }
 
-  const command = require.resolve('@vue/cli-service/bin/vue-cli-service')
-  execa('cross-env', [
-    'NODE_ENV=production',
-    `VUE_APP_GRAPHQL_PORT=${port}`,
-    'VUE_APP_GRAPHQL_ENDPOINT=',
-    'VUE_APP_GRAPHQL_PLAYGROUND_PATH=/graphql-playground',
-    'node',
-    command,
-    'ui'
-  ], {
-    cwd: context,
-    stdio: ['inherit', 'inherit', 'inherit']
-  })
+  // Re-run in production mode
+  if (process.env.NODE_ENV !== 'production') {
+    process.env.VUE_APP_GRAPHQL_PORT = port
+    process.env.VUE_APP_GRAPHQL_ENDPOINT = ''
+    process.env.VUE_APP_GRAPHQL_PLAYGROUND_PATH = '/graphql-playground'
+
+    const command = require.resolve('@vue/cli/bin/vue')
+    execa('cross-env', [
+      'NODE_ENV=production',
+      'node',
+      command,
+      'ui'
+    ], {
+      cwd: context,
+      stdio: ['inherit', 'inherit', 'inherit']
+    })
+  } else {
+    log(`🚀  Starting GUI...`)
+    let server = require('vue-cli-plugin-apollo/graphql-server')
+    server = server.default || server
+
+    const opts = {
+      mock: false,
+      apolloEngine: false,
+      timeout: 999999999,
+      paths: {
+        typeDefs: require.resolve('@vue/cli-ui/src/graphql-api/type-defs.js'),
+        resolvers: require.resolve('@vue/cli-ui/src/graphql-api/resolvers.js'),
+        context: require.resolve('@vue/cli-ui/src/graphql-api/context.js'),
+        pubsub: require.resolve('@vue/cli-ui/src/graphql-api/pubsub.js'),
+        server: require.resolve('@vue/cli-ui/src/graphql-api/server.js')
+      }
+    }
+
+    server(opts, () => {
+      openBrowser(`http://localhost:${process.env.VUE_APP_GRAPHQL_PORT}`)
+    })
+  }
 }
 
 module.exports = (...args) => {
