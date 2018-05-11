c54457ee82a1ca8078b334803619cfc18f4fdeb0
arszp10 <arszp10@gmail.com>
2018-05-11 19:07:31 +0300

    Update HelloWorld.spec.js

--- packages/@vue/cli-plugin-unit-mocha/generator/template/tests/unit/HelloWorld.spec.js
+++ packages/@vue/cli-plugin-unit-mocha/generator/template/tests/unit/HelloWorld.spec.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai'
-import { shallow } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallow(HelloWorld, {
+    const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
     expect(wrapper.text()).to.include(msg)
