f0910ba4cdb392f8400370e3f686a3985bc009cc
Evan You <yyx990803@gmail.com>
2018-05-11 18:39:23 -0400

    refactor: remove experimentalCompileTsWithBabel

--- packages/@vue/cli/lib/promptModules/babel.js
+++ packages/@vue/cli/lib/promptModules/babel.js
@@ -8,7 +8,7 @@ module.exports = cli => {
 
   cli.onPromptComplete((answers, options) => {
     if (answers.features.includes('ts')) {
-      if (!answers.useTsWithBabel && !answers.experimentalCompileTsWithBabel) {
+      if (!answers.useTsWithBabel) {
         return
       }
     } else {
