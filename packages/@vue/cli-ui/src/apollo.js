d64c1a005fd67eb7ad52bba744e3fd72d475c007
Guillaume Chau <alphadelta.fg@gmail.com>
2018-05-19 04:15:08 +0200

    refactor(ui): use only web sockets

--- packages/@vue/cli-ui/src/apollo.js
+++ packages/@vue/cli-ui/src/apollo.js
@@ -1,24 +1,14 @@
 import { ApolloClient } from 'apollo-client'
-import { split, ApolloLink } from 'apollo-link'
-import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
 import { withClientState } from 'apollo-link-state'
 import defaults from './state/defaults'
 import resolvers from './state/resolvers'
 
 // Create the apollo client
 export default function createApolloClient ({ base, endpoints, persisting }) {
-  let link
-  let wsClient
-
-  let httpLink = new HttpLink({
-    // You should use an absolute URL here
-    uri: base + endpoints.graphql
-  })
-
   // Apollo cache
   const cache = new InMemoryCache()
 
@@ -26,7 +16,7 @@ export default function createApolloClient ({ base, endpoints, persisting }) {
   const stateLink = withClientState({ defaults, cache, resolvers })
 
   // Web socket
-  wsClient = new SubscriptionClient(base.replace(/^https?/i, 'ws') +
+  const wsClient = new SubscriptionClient(base.replace(/^https?/i, 'ws') +
   endpoints.subscription, {
     reconnect: true
   })
@@ -34,19 +24,8 @@ export default function createApolloClient ({ base, endpoints, persisting }) {
   // Create the subscription websocket link
   const wsLink = new WebSocketLink(wsClient)
 
-  link = split(
-    // split based on operation type
-    ({ query }) => {
-      const { kind, operation } = getMainDefinition(query)
-      return kind === 'OperationDefinition' &&
-        operation === 'subscription'
-    },
-    wsLink,
-    httpLink
-  )
-
   const apolloClient = new ApolloClient({
-    link: ApolloLink.from([stateLink, link]),
+    link: ApolloLink.from([stateLink, wsLink]),
     cache
   })
 
