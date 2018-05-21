581abd34ef9f113cfa44f7bee19186b636bc8fad
Evan You <yyx990803@gmail.com>
2018-05-21 11:29:31 -0400

    refactor: lintOnSave should be checked by default

--- packages/@vue/cli/lib/promptModules/linter.js
+++ packages/@vue/cli/lib/promptModules/linter.js
@@ -55,7 +55,8 @@ module.exports = cli => {
     choices: [
       {
         name: 'Lint on save',
-        value: 'save'
+        value: 'save',
+        checked: true
       },
       {
         name: 'Lint and fix on commit' + (hasGit() ? '' : chalk.red(' (requires Git)')),
