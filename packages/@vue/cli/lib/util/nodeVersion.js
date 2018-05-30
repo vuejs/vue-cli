0235e3b39a72bac50e28f1ddff9bf2053be94180
Guillaume Chau <alphadelta.fg@gmail.com>
2018-05-30 15:34:05 +0200

    fix: minNode

--- packages/@vue/cli/lib/util/nodeVersion.js
+++ packages/@vue/cli/lib/util/nodeVersion.js
@@ -1,4 +1,13 @@
 exports.minNode = function (major, minor = 0, patch = 0) {
   const [M, m, p] = process.versions.node.split('.').map(v => parseInt(v))
-  return (M >= major && m >= minor && p >= patch)
+  return (
+    M > major ||
+    (
+      M === major &&
+      (
+        m > minor ||
+          (m === minor && p >= patch)
+      )
+    )
+  )
 }
