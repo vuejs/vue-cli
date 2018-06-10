6e1f7357a36dd4c179309cb7afa11eb0d3b6e3f4
Guillaume Chau <guillaume.b.chau@gmail.com>
2018-06-10 17:59:45 +0200

    fix(ui): save db in user home

--- packages/@vue/cli-ui/src/graphql-api/utils/db.js
+++ packages/@vue/cli-ui/src/graphql-api/utils/db.js
@@ -1,19 +1,25 @@
 const Lowdb = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const fs = require('fs-extra')
-const { resolve } = require('path')
+const path = require('path')
+const os = require('os')
+const { xdgConfigPath } = require('@vue/cli/lib/util/xdgConfig')
 
-let folder = '../../../live'
+let folder
 
 if (process.env.VUE_CLI_UI_TEST) {
   folder = '../../../live-test'
   // Clean DB
-  fs.removeSync(resolve(__dirname, folder))
+  fs.removeSync(path.resolve(__dirname, folder))
+} else {
+  folder = process.env.VUE_CLI_UI_DB_PATH ||
+    xdgConfigPath('.vue-cli-ui') ||
+    path.join(os.homedir(), '.vue-cli-ui')
 }
 
-fs.ensureDirSync(resolve(__dirname, folder))
+fs.ensureDirSync(path.resolve(__dirname, folder))
 
-const db = new Lowdb(new FileSync(resolve(__dirname, folder, 'db.json')))
+const db = new Lowdb(new FileSync(path.resolve(__dirname, folder, 'db.json')))
 
 // Seed an empty DB
 db.defaults({
