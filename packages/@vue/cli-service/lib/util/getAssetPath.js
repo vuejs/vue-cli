1195aee3413066e550ab00195032843e83cd5eac
Evan You <yyx990803@gmail.com>
2018-06-11 15:33:22 -0400

    fix: asset output relative check
    close #1545

--- packages/@vue/cli-service/lib/util/getAssetPath.js
+++ packages/@vue/cli-service/lib/util/getAssetPath.js
@@ -3,7 +3,11 @@ const path = require('path')
 module.exports = function getAssetPath (options, filePath, placeAtRootIfRelative) {
   // if the user is using a relative URL, place js & css at dist root to ensure
   // relative paths work properly
-  if (placeAtRootIfRelative && options.baseUrl.charAt(0) !== '/') {
+  if (
+    placeAtRootIfRelative &&
+    !(/^https?:/.test(options.baseUrl)) &&
+    options.baseUrl.charAt(0) !== '/'
+  ) {
     return filePath.replace(/^\w+\//, '')
   }
   return options.assetsDir
