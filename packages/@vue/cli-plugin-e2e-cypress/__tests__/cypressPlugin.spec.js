b4770d0fcb0702f488632ca5e31dc1769b01b51e
Evan You <yyx990803@gmail.com>
2018-06-11 11:12:27 -0400

    ci: temporarily disable Cypress tests on windows

--- packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
+++ packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
@@ -2,17 +2,21 @@ jest.setTimeout(process.env.APPVEYOR ? 80000 : 40000)
 
 const create = require('@vue/cli-test-utils/createTestProject')
 
-test('should work', async () => {
-  const project = await create('e2e-cypress', {
-    plugins: {
-      '@vue/cli-plugin-babel': {},
-      '@vue/cli-plugin-e2e-cypress': {}
-    }
+// temporarily disabled on AppVeyor due to upstream issue
+// https://github.com/cypress-io/cypress/issues/1841
+if (!process.env.APPVEYOR) {
+  test('should work', async () => {
+    const project = await create('e2e-cypress', {
+      plugins: {
+        '@vue/cli-plugin-babel': {},
+        '@vue/cli-plugin-e2e-cypress': {}
+      }
+    })
+
+    const config = JSON.parse(await project.read('cypress.json'))
+    config.video = false
+    await project.write('cypress.json', JSON.stringify(config))
+
+    await project.run(`vue-cli-service test:e2e --headless`)
   })
-
-  const config = JSON.parse(await project.read('cypress.json'))
-  config.video = false
-  await project.write('cypress.json', JSON.stringify(config))
-
-  await project.run(`vue-cli-service test:e2e --headless`)
-})
+}
