cebb2ffbe58f2d49acdf9147c4457fb5ca5c2015
Evan You <yyx990803@gmail.com>
2018-06-18 10:56:36 -0400

    chore: 3.0.0-rc.3 changelog [ci skip]

--- scripts/release.js
+++ scripts/release.js
@@ -4,7 +4,6 @@ const execa = require('execa')
 const semver = require('semver')
 const inquirer = require('inquirer')
 const { syncDeps } = require('./syncDeps')
-const cc = require('conventional-changelog')
 
 const curVersion = require('../lerna.json').version
 
@@ -59,20 +58,7 @@ const release = async () => {
     version
   ], { stdio: 'inherit' })
 
-  const fileStream = require('fs').createWriteStream(`CHANGELOG.md`)
-  cc({
-    preset: 'angular',
-    releaseCount: 0,
-    pkg: {
-      transform (pkg) {
-        pkg.version = `v${version}`
-        return pkg
-      }
-    }
-  }).pipe(fileStream).on('close', async () => {
-    await execa('git', ['add', '-A'], { stdio: 'inherit' })
-    await execa('git', ['commit', '-m', `chore: ${version} changelog [ci skip]`], { stdio: 'inherit' })
-  })
+  require('./genChangelog')(version)
 }
 
 release().catch(err => {
