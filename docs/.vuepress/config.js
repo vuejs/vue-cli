cc9a6c28093568548ebd2b031cadc69357ce522e
Guillaume Chau <alphadelta.fg@gmail.com>
2018-07-14 15:12:14 +0200

    fix: vuepress docs branch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   themeConfig: {
     repo: 'vuejs/vue-cli',
     docsDir: 'docs',
-    docsBranch: 'dev',
+    docsBranch: 'docs',
     editLinks: true,
     sidebarDepth: 3,
     locales: {
