fe117740c4f40a3cffbb77b4fe079dcb0e64fe3b
Evan You <yyx990803@gmail.com>
2018-07-17 15:37:12 -0400

    fix: temporarily disable space-infix-ops for eslint + TS
    ref #1672

--- packages/@vue/eslint-config-typescript/index.js
+++ packages/@vue/eslint-config-typescript/index.js
@@ -6,8 +6,10 @@ module.exports = {
   rules: {
     // https://github.com/eslint/typescript-eslint-parser#known-issues
     'no-undef': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
     // https://github.com/eslint/typescript-eslint-parser/issues/445
-    // 'typescript/no-unused-vars': 'error'
+    // 'typescript/no-unused-vars': 'error',
+    // https://github.com/vuejs/vue-cli/issues/1672
+    'space-infix-ops': 'off'
   }
 }
