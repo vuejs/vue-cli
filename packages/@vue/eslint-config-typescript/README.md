87ad7fc2f853e616527b9dac833bb72f2e05ad09
Evan You <yyx990803@gmail.com>
2018-07-23 13:51:54 -0400

    fix(typescript): avoid error when using ts + eslint + e2e-nightwatch
    close #1922

--- packages/@vue/eslint-config-typescript/README.md
+++ packages/@vue/eslint-config-typescript/README.md
@@ -1,3 +1,5 @@
 # @vue/eslint-config-typescript
 
 > eslint-config-typescript for vue-cli
+
+See [eslint-plugin-typescript](https://github.com/nzakas/eslint-plugin-typescript) for available rules.
--- packages/@vue/eslint-config-typescript/index.js
+++ packages/@vue/eslint-config-typescript/index.js
@@ -10,6 +10,10 @@ module.exports = {
     // https://github.com/eslint/typescript-eslint-parser/issues/445
     // 'typescript/no-unused-vars': 'error',
     // https://github.com/vuejs/vue-cli/issues/1672
-    'space-infix-ops': 'off'
+    'space-infix-ops': 'off',
+    // temporary fix for https://github.com/vuejs/vue-cli/issues/1922
+    // very strange as somehow this rule gets different behaviors depending
+    // on the presence of typescript-eslint-parser...
+    'strict': 'off'
   }
 }
