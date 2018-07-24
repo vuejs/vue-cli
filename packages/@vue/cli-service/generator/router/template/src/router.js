7d46db926a2bc6d8fcda7a103b360c09773e1469
Evan You <yyx990803@gmail.com>
2018-07-24 09:56:05 -0400

    feat: demonstrate route-level code splitting in generated file
    close #1928

--- packages/@vue/cli-service/generator/router/template/src/router.js
+++ packages/@vue/cli-service/generator/router/template/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -15,7 +14,13 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: About
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(
+        /* webpackChunkName: "about" */
+        './views/About.vue'
+      )
     }
   ]
 })
