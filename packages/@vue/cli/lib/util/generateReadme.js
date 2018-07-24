ecd0b8d9244dc3f586b64511aa45626f0c73488c
Evan You <yyx990803@gmail.com>
2018-07-24 11:50:38 -0400

    chore: handle package.scripts being empty when generating readme

--- packages/@vue/cli/lib/util/generateReadme.js
+++ packages/@vue/cli/lib/util/generateReadme.js
@@ -8,7 +8,7 @@ const descriptions = {
 }
 
 function printScripts (pkg, packageManager) {
-  return Object.keys(pkg.scripts).map(key => {
+  return Object.keys(pkg.scripts || {}).map(key => {
     if (!descriptions[key]) return ''
     return [
       `\n### ${descriptions[key]}`,
