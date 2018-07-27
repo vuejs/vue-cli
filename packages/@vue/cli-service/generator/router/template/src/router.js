e27e67923f1204fb58c329fa9ba985ab392e3f06
Evan You <yyx990803@gmail.com>
2018-07-26 21:21:30 -0400

    fix: make sure router.js passes airbnb lint close #1956

--- packages/@vue/cli-service/generator/router/template/src/router.js
+++ packages/@vue/cli-service/generator/router/template/src/router.js
@@ -17,10 +17,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(
-        /* webpackChunkName: "about" */
-        './views/About.vue'
-      )
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
