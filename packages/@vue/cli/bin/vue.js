2a9a0cce5d40bbbe35537d770008f1eb5f13c43b
oika <942093+oika@users.noreply.github.com>
2018-07-31 15:08:38 +0900

    Fix a message in checkNodeVersion.
    From: `this version of vue-clirequires Node >=8.9.`
    To: `this version of vue-cli requires Node >=8.9.`

--- packages/@vue/cli/bin/vue.js
+++ packages/@vue/cli/bin/vue.js
@@ -11,7 +11,7 @@ function checkNodeVersion (wanted, id) {
   if (!semver.satisfies(process.version, wanted)) {
     console.log(chalk.red(
       'You are using Node ' + process.version + ', but this version of ' + id +
-      'requires Node ' + wanted + '.\nPlease upgrade your Node version.'
+      ' requires Node ' + wanted + '.\nPlease upgrade your Node version.'
     ))
     process.exit(1)
   }
