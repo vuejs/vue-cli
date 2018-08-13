d14d4e693c52b6701c173c35da7804dbc32dccc2
Barthélémy Ledoux <ledouxb@me.com>
2018-08-13 11:14:18 -0500

    fix: allow relative baseUrl other than ./ (#2168)

--- packages/@vue/cli-service/__tests__/Service.spec.js
+++ packages/@vue/cli-service/__tests__/Service.spec.js
@@ -94,6 +94,26 @@ test('handle option baseUrl and outputDir correctly', () => {
   expect(service.projectOptions.outputDir).toBe('/public')
 })
 
+test('normalize baseUrl when relative', () => {
+  mockPkg({
+    vue: {
+      baseUrl: './foo/bar'
+    }
+  })
+  const service = createMockService()
+  expect(service.projectOptions.baseUrl).toBe('foo/bar/')
+})
+
+test('keep baseUrl when empty', () => {
+  mockPkg({
+    vue: {
+      baseUrl: ''
+    }
+  })
+  const service = createMockService()
+  expect(service.projectOptions.baseUrl).toBe('')
+})
+
 test('load project options from vue.config.js', () => {
   process.env.VUE_CLI_SERVICE_CONFIG_PATH = `/vue.config.js`
   fs.writeFileSync('/vue.config.js', `module.exports = { lintOnSave: false }`)
