b132a3b313769634c0e092c8e11a182abea57a65
Evan You <yyx990803@gmail.com>
2018-08-15 17:47:58 -0400

    chore: remove outdated warning

--- packages/@vue/cli-plugin-typescript/index.js
+++ packages/@vue/cli-plugin-typescript/index.js
@@ -1,14 +1,4 @@
 module.exports = (api, options) => {
-  // pre-rc7 warning
-  try {
-    require('typescript/package.json')
-  } catch (e) {
-    throw new Error(
-      `"typescript" is now a peer dependency of "@vue/cli-plugin-typescript".\n` +
-      `To fix the build, explicitly install typescript in your project.`
-    )
-  }
-
   const fs = require('fs')
   const useThreads = process.env.NODE_ENV === 'production' && options.parallel
 
