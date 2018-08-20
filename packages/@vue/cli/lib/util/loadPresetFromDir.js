ecb8c1801f7b2410c33422387742c954fc5accb0
Haoqun Jiang <haoqunjiang@gmail.com>
2018-08-20 23:31:04 +0800

    fix: support generator/index.js in local presets (#2263)
    fixes #2172

--- packages/@vue/cli/lib/util/loadPresetFromDir.js
+++ packages/@vue/cli/lib/util/loadPresetFromDir.js
@@ -8,10 +8,10 @@ module.exports = async function loadPresetFromDir (dir) {
   }
   const preset = await fs.readJson(presetPath)
 
-  // if the preset dir contains generator.js, we will inject it as a hidden
+  // if the preset dir contains generator.js or generator/index.js, we will inject it as a hidden
   // plugin so it will be invoked by the generator.
-  const generatorPath = path.join(dir, 'generator.js')
-  if (fs.existsSync(generatorPath)) {
+  const hasGenerator = fs.existsSync(path.join(dir, 'generator.js')) || fs.existsSync(path.join(dir, 'generator/index.js'))
+  if (hasGenerator) {
     (preset.plugins || (preset.plugins = {}))[dir.replace(/[\/]$/, '')] = {
       _isPreset: true,
       prompts: true
