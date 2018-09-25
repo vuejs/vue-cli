5d2033d9814b8262da353dc4599ea9b00dd4236c
Haoqun Jiang <haoqunjiang@gmail.com>
2018-09-25 15:17:39 +0800

    chore: remove trailing comma [ci skip]

--- packages/@vue/cli-plugin-e2e-cypress/generator/template/tests/e2e/plugins/index.js
+++ packages/@vue/cli-plugin-e2e-cypress/generator/template/tests/e2e/plugins/index.js
@@ -4,7 +4,7 @@ const webpack = require('@cypress/webpack-preprocessor')
 module.exports = (on, config) => {
   on('file:preprocessor', webpack({
     webpackOptions: require('@vue/cli-service/webpack.config'),
-    watchOptions: {},
+    watchOptions: {}
   }))
 
   return Object.assign({}, config, {
