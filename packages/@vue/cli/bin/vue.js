54b9a92ee308f24a6547a0c3ee1a15126dfaef6d
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-09-26 11:02:55 +0900

    Change shorthand of --host to -H

--- packages/@vue/cli/bin/vue.js
+++ packages/@vue/cli/bin/vue.js
@@ -115,7 +115,7 @@ program
 program
   .command('ui')
   .description('start and open the vue-cli ui')
-  .option('-h, --host <host>', 'Host used for the UI server (default: localhost)')
+  .option('-H, --host <host>', 'Host used for the UI server (default: localhost)')
   .option('-p, --port <port>', 'Port used for the UI server (by default search for available port)')
   .option('-D, --dev', 'Run in dev mode')
   .option('--quiet', `Don't output starting messages`)
