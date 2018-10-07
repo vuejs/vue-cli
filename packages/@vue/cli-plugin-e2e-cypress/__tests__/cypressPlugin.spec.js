c0ca45cdaa24737e787c76f76d08e462f284addc
Haoqun Jiang <haoqunjiang@gmail.com>
2018-10-07 17:58:35 +0800

    test: run cypress in headless mode only when in CI env
    Some issues only happens in GUI mode (https://github.com/vuejs/vue-cli/pull/2672),
    so we need to manually identify these issues during development.

--- packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
+++ packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
@@ -2,28 +2,24 @@ jest.setTimeout(process.env.APPVEYOR ? 80000 : 60000)
 
 const create = require('@vue/cli-test-utils/createTestProject')
 
-// temporarily disabled on AppVeyor due to upstream issue
-// https://github.com/cypress-io/cypress/issues/1841
-if (!process.env.APPVEYOR) {
-  test('should work', async () => {
-    const project = await create('e2e-cypress', {
-      plugins: {
-        '@vue/cli-plugin-babel': {},
-        '@vue/cli-plugin-e2e-cypress': {}
-      }
-    })
+test('should work', async () => {
+  const project = await create('e2e-cypress', {
+    plugins: {
+      '@vue/cli-plugin-babel': {},
+      '@vue/cli-plugin-e2e-cypress': {}
+    }
+  })
 
-    const pkg = JSON.parse(await project.read('package.json'))
-    expect(pkg.devDependencies).toHaveProperty('@cypress/webpack-preprocessor')
+  const pkg = JSON.parse(await project.read('package.json'))
+  expect(pkg.devDependencies).toHaveProperty('@cypress/webpack-preprocessor')
 
-    const config = JSON.parse(await project.read('cypress.json'))
-    config.video = false
-    await project.write('cypress.json', JSON.stringify(config))
+  const config = JSON.parse(await project.read('cypress.json'))
+  config.video = false
+  await project.write('cypress.json', JSON.stringify(config))
 
+  if (process.env.CI) {
     await project.run(`vue-cli-service test:e2e --headless`)
-  })
-} else {
-  test('should work', () => {
-    // empty spec to avoid jest bailing
-  })
-}
+  } else {
+    await project.run(`vue-cli-service test:e2e`)
+  }
+})
