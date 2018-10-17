fa21cfa42153850aa248ddc5fbdcef4823a42436
Guillaume Chau <alphadelta.fg@gmail.com>
2018-10-17 10:57:15 -0700

    feat(news feed): handle errors

--- packages/@vue/cli-ui-addon-widgets/src/components/News.vue
+++ packages/@vue/cli-ui-addon-widgets/src/components/News.vue
@@ -7,6 +7,13 @@
       <VueLoadingIndicator/>
     </div>
     <div
+      v-else-if="error"
+      class="error vue-ui-empty"
+    >
+      <VueIcon :icon="errorData.icon" class="huge"/>
+      <div>{{ $t(`org.vue.widgets.news.errors.${error}`) }}</div>
+    </div>
+    <div
       v-else
       class="feed"
     >
@@ -28,6 +35,18 @@ const parser = new Parser()
 
 const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
 
+const ERRORS = {
+  'fetch': {
+    icon: 'error'
+  },
+  'offline': {
+    icon: 'cloud_off'
+  },
+  'empty': {
+    icon: 'cake'
+  }
+}
+
 export default {
   components: {
     NewsItem
@@ -40,7 +59,16 @@ export default {
   data () {
     return {
       loading: false,
-      feed: null
+      feed: null,
+      error: null
+    }
+  },
+
+  computed: {
+    errorData () {
+      if (this.error) {
+        return ERRORS[this.error]
+      }
     }
   },
 
@@ -53,10 +81,21 @@ export default {
 
   methods: {
     async fetchFeed () {
+      if (!navigator.onLine) {
+        this.loading = false
+        this.error = 'offline'
+        return
+      }
+
       this.loading = true
+      this.error = false
       try {
         this.feed = await parser.parseURL(CORS_PROXY + this.widget.data.config.url)
+        if (!this.feed.items.length) {
+          this.error = 'empty'
+        }
       } catch (e) {
+        this.error = 'fetch'
         console.error(e)
       }
       this.loading = false
@@ -79,4 +118,10 @@ export default {
 .feed
   overflow-x hidden
   overflow-y auto
+
+.error
+  height 100%
+  v-box()
+  box-center()
+  padding-bottom 42px
 </style>
