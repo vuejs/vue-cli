c50051262f4217dc0faa7883912b2e816798070d
Michał Sajnóg <msajnog93@gmail.com>
2018-10-29 12:15:46 +0100

    fix(cli-plugin-eslint): Resolve proper ESLint package (#2560)

--- packages/@vue/cli-plugin-eslint/index.js
+++ packages/@vue/cli-plugin-eslint/index.js
@@ -3,6 +3,9 @@ const path = require('path')
 module.exports = (api, options) => {
   if (options.lintOnSave) {
     const extensions = require('./eslintOptions').extensions(api)
+    const { loadModule } = require('@vue/cli-shared-utils')
+    const cwd = api.getCwd()
+    const eslintPkg = loadModule('eslint/package.json', cwd, true)
 
     // eslint-loader doesn't bust cache when eslint config changes
     // so we have to manually generate a cache identifier that takes the config
@@ -11,7 +14,7 @@ module.exports = (api, options) => {
       'eslint-loader',
       {
         'eslint-loader': require('eslint-loader/package.json').version,
-        'eslint': require('eslint/package.json').version
+        'eslint': eslintPkg.version
       },
       [
         '.eslintrc.js',
@@ -42,7 +45,7 @@ module.exports = (api, options) => {
               cacheIdentifier,
               emitWarning: options.lintOnSave !== 'error',
               emitError: options.lintOnSave === 'error',
-              formatter: require('eslint/lib/formatters/codeframe')
+              formatter: loadModule('eslint/lib/formatters/codeframe', cwd, true)
             })
     })
   }
