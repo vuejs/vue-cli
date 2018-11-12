e4e151b238c339b10492453fca3431435c0dd85d
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2018-11-12 07:44:46 +0100

    fix: remove webpack-preprocessor from cypress config (#2904)
    * test: add cypress test for TS
    
    * fix: remove webpack-preprocessor from cypress config
    
    Removes the `@cypress/webpack-preprocessor` from the generated cypress configuration, as it leads to several issues regarding file watching, headless mode and TS support.
    
    Fixes #2903

--- packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
+++ packages/@vue/cli-plugin-e2e-cypress/__tests__/cypressPlugin.spec.js
@@ -14,8 +14,28 @@ test('should work', async () => {
     }
   })
 
-  const pkg = JSON.parse(await project.read('package.json'))
-  expect(pkg.devDependencies).toHaveProperty('@cypress/webpack-preprocessor')
+  const config = JSON.parse(await project.read('cypress.json'))
+  config.video = false
+  await project.write('cypress.json', JSON.stringify(config))
+
+  if (!process.env.CI) {
+    await project.run(`vue-cli-service test:e2e`)
+  } else if (!process.env.APPVEYOR) {
+    await project.run(`vue-cli-service test:e2e --headless`)
+  }
+})
+
+test('should work with TS', async () => {
+  const project = await create('e2e-cypress-ts', {
+    plugins: {
+      '@vue/cli-plugin-typescript': {
+        'classComponent': true,
+        'tsLint': true,
+        'lintOn': ['save']
+      },
+      '@vue/cli-plugin-e2e-cypress': {}
+    }
+  })
 
   const config = JSON.parse(await project.read('cypress.json'))
   config.video = false
