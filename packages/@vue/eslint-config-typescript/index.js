076c365748e0e8eb27e9b85b7ae0250afdfd4e2f
Haoqun Jiang <haoqunjiang@gmail.com>
2018-11-21 23:58:23 +0800

    chore: no longer needs to disable `space-infix-ops`
    Now that the bug in typescript-eslint-parser has been fixed.

--- packages/@vue/eslint-config-typescript/index.js
+++ packages/@vue/eslint-config-typescript/index.js
@@ -13,8 +13,7 @@ module.exports = {
     'no-unused-vars': 'off',
     // https://github.com/eslint/typescript-eslint-parser/issues/445
     // 'typescript/no-unused-vars': 'error',
-    // https://github.com/vuejs/vue-cli/issues/1672
-    'space-infix-ops': 'off',
+
     // temporary fix for https://github.com/vuejs/vue-cli/issues/1922
     // very strange as somehow this rule gets different behaviors depending
     // on the presence of typescript-eslint-parser...
