728bff026f557e8c32812408d82339d1e0b198d4
Iulian Onofrei <6d0847b9@opayq.com>
2018-11-27 23:20:44 +0200

    Make the demo msg property public

--- packages/@vue/cli-plugin-typescript/generator/template/src/components/HelloWorld.vue
+++ packages/@vue/cli-plugin-typescript/generator/template/src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop() public msg!: string;
 }
 <%_ } _%>
 </script>
