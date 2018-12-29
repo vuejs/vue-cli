d86b9d4c1f0b6bbfaf241918f6494513709e533e
Haoqun Jiang <haoqunjiang@gmail.com>
2018-12-29 14:54:39 +0800

    chore: add deprecation warning for Node.js 9.x (#3202)
    * chore: add deprecation warning for Node.js 9.x
    
    * chore: upgrade -> use an active LTS version

--- packages/@vue/cli/bin/vue.js
+++ packages/@vue/cli/bin/vue.js
@@ -19,6 +19,14 @@ function checkNodeVersion (wanted, id) {
 
 checkNodeVersion(requiredVersion, 'vue-cli')
 
+if (semver.satisfies(process.version, '9.x')) {
+  console.log(chalk.red(
+    `You are using Node ${process.version}.\n` +
+    `Node.js 9.x has already reached end-of-life and will not be supported in future major releases.\n` +
+    `It's strongly recommended to use an active LTS version instead.`
+  ))
+}
+
 const fs = require('fs')
 const path = require('path')
 const slash = require('slash')
