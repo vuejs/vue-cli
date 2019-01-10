6d5cbdb4793ab4f3c591fc76885384658fb46b82
Haoqun Jiang <haoqunjiang@gmail.com>
2019-01-10 16:46:11 +0800

    docs: host bit's logo in the project [ci skip]
    fixes #3273

--- docs/.vuepress/components/Bit.vue
+++ docs/.vuepress/components/Bit.vue
@@ -2,7 +2,7 @@
   <p class="bit-sponsor">
     <a href="https://www.bitsrc.io/?utm_source=vue&utm_medium=vue&utm_campaign=vue&utm_term=vue&utm_content=vue" target="_blank">
       <span>This project is sponsored by</span>
-      <img alt="bit" src="https://raw.githubusercontent.com/vuejs/vuejs.org/master/themes/vue/source/images/bit-wide.png">
+      <img alt="bit" src="/bit-wide.png">
     </a>
   </p>
 </template>
