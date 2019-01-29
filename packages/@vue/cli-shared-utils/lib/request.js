09be0f2e1fa37a39ca5c83c8a2f1320d40f0bab6
Haoqun Jiang <haoqunjiang@gmail.com>
2019-01-29 23:10:17 +0800

    fix: add timeout for requests
    closes #3076

--- packages/@vue/cli-shared-utils/lib/request.js
+++ packages/@vue/cli-shared-utils/lib/request.js
@@ -4,6 +4,7 @@ exports.request = {
     const request = require('request-promise-native')
     const reqOpts = {
       method: 'GET',
+      timeout: 30000,
       resolveWithFullResponse: true,
       json: true,
       uri
