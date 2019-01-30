fb71653b27631d2dc718a7772ac0eddfc845de91
JK <jingkai.zhao@foxmail.com>
2019-01-31 00:59:52 +0800

    fix(cli-plugin-babel): transpileDependencies should only match packages inside `node_modules`, close #3057 (#3229)

--- packages/@vue/cli-plugin-babel/index.js
+++ packages/@vue/cli-plugin-babel/index.js
@@ -1,8 +1,24 @@
 const path = require('path')
+const { isWindows } = require('@vue/cli-shared-utils')
+
+function genTranspileDepRegex (transpileDependencies) {
+  const deps = transpileDependencies.map(dep => {
+    if (typeof dep === 'string') {
+      const depPath = path.join('node_modules', dep, '/')
+      return isWindows
+        ? depPath.replace(/\\/g, '\\\\') // double escape for windows style path
+        : depPath
+    } else if (dep instanceof RegExp) {
+      return dep.source
+    }
+  })
+  return deps.length ? new RegExp(deps.join('|')) : null
+}
 
 module.exports = (api, options) => {
   const useThreads = process.env.NODE_ENV === 'production' && options.parallel
   const cliServicePath = require('path').dirname(require.resolve('@vue/cli-service'))
+  const transpileDepRegex = genTranspileDepRegex(options.transpileDependencies)
 
   api.chainWebpack(webpackConfig => {
     webpackConfig.resolveLoader.modules.prepend(path.join(__dirname, 'node_modules'))
@@ -21,13 +37,7 @@ module.exports = (api, options) => {
               return true
             }
             // check if this is something the user explicitly wants to transpile
-            if (options.transpileDependencies.some(dep => {
-              if (typeof dep === 'string') {
-                return filepath.includes(path.normalize(dep))
-              } else {
-                return filepath.match(dep)
-              }
-            })) {
+            if (transpileDepRegex && transpileDepRegex.test(filepath)) {
               return false
             }
             // Don't transpile node_modules
