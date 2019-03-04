9bfe759e2492480349a9490da2ee993c4a5d788c
Haoqun Jiang <haoqunjiang@gmail.com>
2019-03-04 17:46:25 +0800

    refactor: use an environment variable to determine the entry files to inject default polyfills
    The old logic is not reliable due to the presence of thread-loader

--- packages/@vue/babel-preset-app/index.js
+++ packages/@vue/babel-preset-app/index.js
@@ -30,6 +30,7 @@ function getPolyfills (targets, includes, { ignoreBrowserslistConfig, configPath
 module.exports = (context, options = {}) => {
   const presets = []
   const plugins = []
+  const defaultEntryFiles = JSON.parse(process.env.VUE_CLI_ENTRY_FILES || '[]')
 
   // JSX
   if (options.jsx !== false) {
@@ -53,7 +54,8 @@ module.exports = (context, options = {}) => {
     decoratorsBeforeExport,
     decoratorsLegacy,
     // entry file list
-    entryFiles,
+    // TODO:
+    entryFiles = defaultEntryFiles,
 
     // Undocumented option of @babel/plugin-transform-runtime.
     // When enabled, an absolute path is used when importing a runtime helper atfer tranforming.
