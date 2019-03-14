bbd573bb755097d504514c50ef0e91bfb7f76026
Haoqun Jiang <haoqunjiang@gmail.com>
2019-03-14 14:16:19 +0800

    docs: add manifest in html head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,14 @@ module.exports = {
   },
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: {
     '@vuepress/pwa': {
