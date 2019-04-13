8b24b51b4de051fdc80f919e206d7ceac3202709
Haoqun Jiang <haoqunjiang@gmail.com>
2019-04-14 01:17:01 +0800

    workflow: fix changelog version title

--- scripts/genChangelog.js
+++ scripts/genChangelog.js
@@ -3,7 +3,11 @@ const path = require('path')
 const execa = require('execa')
 
 async function genNewRelease () {
-  const { stdout } = await execa(require.resolve('lerna-changelog/bin/cli'))
+  const nextVersion = require('../lerna.json').version
+  const { stdout } = await execa(require.resolve('lerna-changelog/bin/cli'), [
+    '--next-version',
+    nextVersion
+  ])
   return stdout
 }
 
@@ -11,7 +15,8 @@ const gen = (module.exports = async () => {
   const newRelease = await genNewRelease()
   const changelogPath = path.resolve(__dirname, '../CHANGELOG.md')
 
-  const newChangelog = newRelease + '\n\n\n' + fs.readFileSync(changelogPath, { encoding: 'utf8' })
+  const newChangelog =
+    newRelease + '\n\n\n' + fs.readFileSync(changelogPath, { encoding: 'utf8' })
   fs.writeFileSync(changelogPath, newChangelog)
 
   delete process.env.PREFIX
