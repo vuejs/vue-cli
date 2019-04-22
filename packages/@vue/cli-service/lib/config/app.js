220c2a00202af8d27d47dddea192e8ecb546cc3d
Haoqun Jiang <haoqunjiang@gmail.com>
2019-04-22 17:24:28 +0800

    fix: fix mocha tests

--- packages/@vue/cli-service/lib/config/app.js
+++ packages/@vue/cli-service/lib/config/app.js
@@ -24,24 +24,26 @@ module.exports = (api, options) => {
     const outputDir = api.resolve(options.outputDir)
 
     // code splitting
-    webpackConfig
-      .optimization.splitChunks({
-        cacheGroups: {
-          vendors: {
-            name: `chunk-vendors`,
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            chunks: 'initial'
-          },
-          common: {
-            name: `chunk-common`,
-            minChunks: 2,
-            priority: -20,
-            chunks: 'initial',
-            reuseExistingChunk: true
+    if (process.env.NODE_ENV !== 'test') {
+      webpackConfig
+        .optimization.splitChunks({
+          cacheGroups: {
+            vendors: {
+              name: `chunk-vendors`,
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              chunks: 'initial'
+            },
+            common: {
+              name: `chunk-common`,
+              minChunks: 2,
+              priority: -20,
+              chunks: 'initial',
+              reuseExistingChunk: true
+            }
           }
-        }
-      })
+        })
+    }
 
     // HTML plugin
     const resolveClientEnv = require('../util/resolveClientEnv')
