b916635d9e97933e28685dd7dab14bbca4d5fe92
Evan You <yyx990803@gmail.com>
2019-05-08 08:59:46 +0800

    chore: update sponsor link

--- docs/.vuepress/components/Bit.vue
+++ docs/.vuepress/components/Bit.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="bit-sponsor">
-    <a href="https://www.bitsrc.io/?utm_source=vue&utm_medium=vue&utm_campaign=vue&utm_term=vue&utm_content=vue" target="_blank">
+    <a href="https://bit.dev/?utm_source=vue&utm_medium=vue&utm_campaign=vue&utm_term=vue&utm_content=vue" target="_blank">
       <span>This project is sponsored by</span>
       <img alt="bit" src="/bit-wide.png">
     </a>
