7a79247997e924cab001f823fb4f195cf91e2fc0
Natalia Tepluhina <tarya.se@gmail.com>
2019-06-11 22:54:54 +0300

    docs: removed hardcoded plugins list

--- scripts/genDocs.js
+++ scripts/genDocs.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const path = require('path')
 
-const plugins = ['babel', 'e2e-cypress', 'e2e-nightwatch', 'eslint', 'pwa', 'typescript', 'unit-jest', 'unit-mocha']
+const pluginsDirPath = path.resolve(__dirname, '../packages', '@vue')
+const pluginRegEx = new RegExp('cli-plugin-')
+
+function generatePluginDoc (plugin) {
+  const entryPath = path.resolve(__dirname, '../packages', '@vue', plugin, 'README.md')
+  const entryContent = fs.readFileSync(entryPath)
+  const docPath = path.resolve(__dirname, '../docs', 'core-plugins', `${plugin.replace('cli-plugin-', '')}.md`)
+  fs.writeFile(docPath, entryContent, () => { })
+}
 
 const genDocs = (module.exports = async () => {
-  plugins.forEach(plugin => {
-    const entryPath = path.resolve(__dirname, '../packages', '@vue', `cli-plugin-${plugin}`, 'README.md')
-    const entryContent = fs.readFileSync(entryPath)
-    const docPath = path.resolve(__dirname, '../docs', 'core-plugins', `${plugin}.md`)
-    fs.writeFile(docPath, entryContent, () => {})
+  fs.readdir(pluginsDirPath, (_, files) => {
+    files.forEach(file => {
+      if (pluginRegEx.test(file)) {
+        generatePluginDoc(file)
+      }
+    })
   })
 })
 
