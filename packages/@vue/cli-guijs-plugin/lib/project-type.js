2276aedd37d4ac55f2789079c47545557b2bb3a6
Guillaume Chau <alphadelta.fg@gmail.com>
2019-07-11 17:49:06 +0200

    fix: better project type logo

--- packages/@vue/cli-guijs-plugin/lib/project-type.js
+++ packages/@vue/cli-guijs-plugin/lib/project-type.js
@@ -1,5 +1,7 @@
 module.exports = api => {
   api.addProjectType('vue', config => {
+    config.logo = '/_plugin/@vue%2Fcli-guijs-plugin/vue-project.png'
+
     // Detect Vue CLI project
     config.filterProject(({ pkg }) => ({ ...pkg.dependencies, ...pkg.devDependencies })['@vue/cli-service'])
   })
