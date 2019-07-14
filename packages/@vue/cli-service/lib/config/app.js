9faddce63fe5a98eb51eafd87a2680bf7c2aefc5
shadow-light <42055707+shadow-light@users.noreply.github.com>
2019-07-15 00:39:31 +1000

    fix: indexPath should also affect non-production builds (#4057)
    fixes #2327
    fixes #4247

--- packages/@vue/cli-service/lib/config/app.js
+++ packages/@vue/cli-service/lib/config/app.js
@@ -92,20 +92,20 @@ module.exports = (api, options) => {
       }
     }
 
-    if (isProd) {
-      // handle indexPath
-      if (options.indexPath !== 'index.html') {
-        // why not set filename for html-webpack-plugin?
-        // 1. It cannot handle absolute paths
-        // 2. Relative paths causes incorrect SW manifest to be generated (#2007)
-        webpackConfig
-          .plugin('move-index')
-          .use(require('../webpack/MovePlugin'), [
-            path.resolve(outputDir, 'index.html'),
-            path.resolve(outputDir, options.indexPath)
-          ])
-      }
+    // handle indexPath
+    if (options.indexPath !== 'index.html') {
+      // why not set filename for html-webpack-plugin?
+      // 1. It cannot handle absolute paths
+      // 2. Relative paths causes incorrect SW manifest to be generated (#2007)
+      webpackConfig
+        .plugin('move-index')
+        .use(require('../webpack/MovePlugin'), [
+          path.resolve(outputDir, 'index.html'),
+          path.resolve(outputDir, options.indexPath)
+        ])
+    }
 
+    if (isProd) {
       Object.assign(htmlOptions, {
         minify: {
           removeComments: true,
