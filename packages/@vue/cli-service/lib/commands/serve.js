3dd3774346ac4d90f251bc4ebb65044e048c2abf
Haoqun Jiang <haoqunjiang@gmail.com>
2019-07-18 16:36:53 +0800

    chore!: move dev configs into serve command (#4302)
    BREAKING CHANGE:
    These configs are only meaningful when used with a running dev
    server.
    Technically, it is a breaking change.
    For example, this breaks use cases in which a user runs their own dev
    server instead of calling `vue-cli-service serve`.

--- packages/@vue/cli-service/lib/commands/serve.js
+++ packages/@vue/cli-service/lib/commands/serve.js
@@ -44,6 +44,30 @@ module.exports = (api, options) => {
     const validateWebpackConfig = require('../util/validateWebpackConfig')
     const isAbsoluteUrl = require('../util/isAbsoluteUrl')
 
+    // configs that only matters for dev server
+    api.chainWebpack(webpackConfig => {
+      if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+        webpackConfig
+          .devtool('cheap-module-eval-source-map')
+
+        webpackConfig
+          .plugin('hmr')
+            .use(require('webpack/lib/HotModuleReplacementPlugin'))
+
+        // https://github.com/webpack/webpack/issues/6642
+        // https://github.com/vuejs/vue-cli/issues/3539
+        webpackConfig
+          .output
+            .globalObject(`(typeof self !== 'undefined' ? self : this)`)
+
+        if (!process.env.VUE_CLI_TEST && options.devServer.progress !== false) {
+          webpackConfig
+            .plugin('progress')
+            .use(require('webpack/lib/ProgressPlugin'))
+        }
+      }
+    })
+
     // resolve webpack config
     const webpackConfig = api.resolveWebpackConfig()
 
