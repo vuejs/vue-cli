fbe287d966601933e2d99913a209b4ff58cf2f4c
Haoqun Jiang <haoqunjiang@gmail.com>
2019-08-08 10:41:44 +0800

    test: fix visibility issues in Cypress test

--- packages/@vue/cli-ui/tests/e2e/specs/g2-plugins.js
+++ packages/@vue/cli-ui/tests/e2e/specs/g2-plugins.js
@@ -9,12 +9,14 @@ describe('Plugins', () => {
     cy.get('[data-testid="add-plugin"]').click()
     cy.get('.project-plugins-add').should('be.visible')
     // Search
+    cy.get('.instant-search-input input').clear().type('pwa')
     cy.get('.package-search-item:contains("@vue/cli-plugin-pwa")').should('be.visible')
     cy.get('.instant-search-input input').clear().type('unit-jest')
     cy.get('.package-search-item:contains("@vue/cli-plugin-pwa")').should('be.not.visible')
     cy.get('.package-search-item:contains("@vue/cli-plugin-unit-jest")').should('be.visible')
     cy.get('.instant-search-input input').clear()
     // Install
+    cy.get('.instant-search-input input').clear().type('pwa')
     cy.get('.package-search-item:contains("@vue/cli-plugin-pwa") [data-testid="name"]').should('be.visible').click()
     cy.get('[data-testid="download-plugin"]:contains("@vue/cli-plugin-pwa")').should('not.have.class', 'disabled').click()
     cy.get('.loading-screen .vue-ui-loading-indicator').should('be.visible')
