c8e45dc6b66921862e5a2cdb3ed0113c37ded459
Haoqun Jiang <haoqunjiang@gmail.com>
2019-09-04 01:20:22 +0800

    fix: correctly sort test scripts in package.json
    The previous `keyOrder` referred to the legacy `test` & `e2e` commands
    that were removed / deprecated since 3.0 stable
    
    (cherry picked from commit 0ca37b745340248c97db1259681cb36c4c445359)

--- packages/@vue/cli/lib/Generator.js
+++ packages/@vue/cli/lib/Generator.js
@@ -170,8 +170,8 @@ module.exports = class Generator {
     this.pkg.scripts = sortObject(this.pkg.scripts, [
       'serve',
       'build',
-      'test',
-      'e2e',
+      'test:unit',
+      'test:e2e',
       'lint',
       'deploy'
     ])
