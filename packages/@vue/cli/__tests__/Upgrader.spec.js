74fae4454b1fc5167beb3e2eba1c59ba3343fed4
Haoqun Jiang <haoqunjiang@gmail.com>
2019-09-07 12:56:30 +0800

    feat: implement a migrator to auto add eslint to deps (#4549)

--- packages/@vue/cli/__tests__/Upgrader.spec.js
+++ packages/@vue/cli/__tests__/Upgrader.spec.js
@@ -15,8 +15,11 @@ beforeAll(() => {
   }
 })
 
-test('upgrade: plugin-babel v3.5', async () => {
+beforeEach(() => {
   process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN = true
+})
+
+test('upgrade: plugin-babel v3.5', async () => {
   const project = await create('plugin-babel-legacy', {
     plugins: {
       '@vue/cli-plugin-babel': {
@@ -28,7 +31,7 @@ test('upgrade: plugin-babel v3.5', async () => {
   const pkg = JSON.parse(await project.read('package.json'))
   expect(pkg.dependencies).not.toHaveProperty('core-js')
 
-  await (new Upgrader(project.dir)).upgrade('@vue/babel', {})
+  await (new Upgrader(project.dir)).upgrade('babel', {})
 
   const updatedPkg = JSON.parse(await project.read('package.json'))
   expect(updatedPkg.dependencies).toHaveProperty('core-js')
@@ -40,7 +43,6 @@ test('upgrade: plugin-babel v3.5', async () => {
 })
 
 test('upgrade: plugin-babel with core-js 2', async () => {
-  process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN = true
   const project = await create('plugin-babel-v3', {
     plugins: {
       '@vue/cli-plugin-babel': {
@@ -52,8 +54,26 @@ test('upgrade: plugin-babel with core-js 2', async () => {
   const pkg = JSON.parse(await project.read('package.json'))
   expect(pkg.dependencies['core-js']).toMatch('^2')
 
-  await (new Upgrader(project.dir)).upgrade('@vue/babel', {})
+  await (new Upgrader(project.dir)).upgrade('babel', {})
 
   const updatedPkg = JSON.parse(await project.read('package.json'))
   expect(updatedPkg.dependencies['core-js']).toMatch('^3')
+})
+
+test('upgrade: should add eslint to devDependencies', async () => {
+  const project = await create('plugin-eslint-v3.0', {
+    plugins: {
+      '@vue/cli-plugin-eslint': {
+        version: '3.0.0'
+      }
+    }
+  }, outsideTestFolder)
+
+  const pkg = JSON.parse(await project.read('package.json'))
+  expect(pkg.devDependencies).not.toHaveProperty('eslint')
+
+  await (new Upgrader(project.dir)).upgrade('eslint', {})
+
+  const updatedPkg = JSON.parse(await project.read('package.json'))
+  expect(updatedPkg.devDependencies.eslint).toMatch('^4')
 })
