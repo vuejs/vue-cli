c0baf059602fa4d828f635a9fa04031925d90abf
Haoqun Jiang <haoqunjiang@gmail.com>
2019-09-30 14:02:48 +0800

    fix: fix sassOptions merging for scss syntax in sass-loader v8 (#4631)
    fixes #4630
    
    (cherry picked from commit ebdb142a4b61f9b94279a5f4e7e654500f75fbcf)

--- packages/@vue/cli-service/lib/config/css.js
+++ packages/@vue/cli-service/lib/config/css.js
@@ -196,7 +196,8 @@ module.exports = (api, options) => {
         loaderOptions.sass,
         {
           sassOptions: Object.assign(
-            (loaderOptions.sass && loaderOptions.sass.sassOptions) || {},
+            {},
+            loaderOptions.sass && loaderOptions.sass.sassOptions,
             {
               indentedSyntax: true
             }
