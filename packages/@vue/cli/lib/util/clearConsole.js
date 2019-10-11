9af329e5f202e044c3b7c5d4cac9f658386813b4
Haoqun Jiang <haoqunjiang@gmail.com>
2019-10-11 15:19:24 +0800

    feat: try to find the right command to update

--- packages/@vue/cli/lib/util/clearConsole.js
+++ packages/@vue/cli/lib/util/clearConsole.js
@@ -1,7 +1,29 @@
 const chalk = require('chalk')
+const execa = require('execa')
 const semver = require('semver')
 const getVersions = require('./getVersions')
-const { clearConsole } = require('@vue/cli-shared-utils')
+const { clearConsole, hasYarn, hasPnpm3OrLater } = require('@vue/cli-shared-utils')
+
+async function getInstallationCommand () {
+  if (hasYarn()) {
+    const { stdout: yarnGlobalDir } = await execa('yarn', ['global', 'dir'])
+    if (__dirname.includes(yarnGlobalDir)) {
+      return 'yarn global add'
+    }
+  }
+
+  if (hasPnpm3OrLater()) {
+    const { stdout: pnpmGlobalPrefix } = await execa('pnpm', ['config', 'get', 'prefix'])
+    if (__dirname.includes(pnpmGlobalPrefix) && __dirname.includes('pnpm-global')) {
+      return `pnpm i -g`
+    }
+  }
+
+  const { stdout: npmGlobalPrefix } = await execa('npm', ['config', 'get', 'prefix'])
+  if (__dirname.includes(npmGlobalPrefix)) {
+    return `npm i -g`
+  }
+}
 
 exports.generateTitle = async function (checkUpdate) {
   const { current, latest } = await getVersions()
@@ -18,8 +40,21 @@ exports.generateTitle = async function (checkUpdate) {
     if (process.env.VUE_CLI_API_MODE) {
       title += chalk.green(` 🌟️ New version available: ${latest}`)
     } else {
-      const upgradeMessage = `New version available ${chalk.magenta(current)} → ${chalk.green(latest)}\n` +
-        `Run ${chalk.yellow(`npm i -g ${require('../../package.json').name}`)} to update!`
+      let upgradeMessage = `New version available ${chalk.magenta(current)} → ${chalk.green(latest)}`
+
+      try {
+        const command = await getInstallationCommand()
+        let name = require('../../package.json').name
+        if (semver.prerelease(latest)) {
+          name += '@next'
+        }
+
+        if (command) {
+          upgradeMessage +=
+            `\nRun ${chalk.yellow(`${command} ${name}`)} to update!`
+        }
+      } catch (e) {}
+
       const upgradeBox = require('boxen')(upgradeMessage, {
         align: 'center',
         borderColor: 'green',
