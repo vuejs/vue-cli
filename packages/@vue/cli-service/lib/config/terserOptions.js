f3b8611df1ac88ece8bd33dc0cd00e8d6926114a
Haoqun Jiang <haoqunjiang@gmail.com>
2019-10-11 13:37:17 +0800

    chore!: upgrade terser-webpack-plugin to 2.x
    BREAKING CHANGE:
    The only real breaking change is https://github.com/webpack-contrib/terser-webpack-plugin/issues/129,
    which is not likely to affect normal users.
    
    The rest are default option values changes, which are already covered in
    the default provided `terserOptions` of Vue CLI.
    
    Note that here we choose to disable `extractComments` by default, as I
    don't see enough data showing such license comments taking too much
    space. The extra LICENSE file may also be confusing to some users.

--- packages/@vue/cli-service/lib/config/terserOptions.js
+++ packages/@vue/cli-service/lib/config/terserOptions.js
@@ -37,5 +37,6 @@ module.exports = options => ({
   },
   sourceMap: options.productionSourceMap,
   cache: true,
-  parallel: options.parallel
+  parallel: options.parallel,
+  extractComments: false
 })
