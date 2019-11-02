2af12b5256556e2063440f13931ea9c25d305db3
Manuel Ojeda <manuelojedasistemas@gmail.com>
2019-11-01 22:17:54 -0700

    types(router): added router array type for Array RouteConfig

--- packages/@vue/cli-plugin-router/generator/template/src/router/index.js
+++ packages/@vue/cli-plugin-router/generator/template/src/router/index.js
@@ -1,9 +1,35 @@
 import Vue from 'vue'
+<%_ if (hasTypeScript) { _%>
+import VueRouter, { RouteConfig } from 'vue-router'
+<%_ } else { _%>
 import VueRouter from 'vue-router'
+<%_ } _%>
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+<%_ if (hasTypeScript) { _%>
+const routes = <Array<RouteConfig>>[
 
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    <%_ if (doesCompile) { _%>
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    <%_ } else { _%>
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+    <%_ } _%>
+  }
+]
+<%_ } else { _%>
 const routes = [
   {
     path: '/',
@@ -25,6 +51,9 @@ const routes = [
     <%_ } _%>
   }
 ]
+<%_ } _%>
+
+Vue.use(VueRouter)
 
 const router = new VueRouter({
   <%_ if (historyMode) { _%>
