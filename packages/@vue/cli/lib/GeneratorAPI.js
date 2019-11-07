1e663df1cab36de10e95b6691a9ba95a8b707aa0
Haoqun Jiang <haoqunjiang@gmail.com>
2019-11-07 10:29:06 +0800

    feat(GeneratorAPI): accept multiple arguments for the resolve method (#4715)
    * feat(GeneratorAPI): accept multiple arguments for the resolve method
    
    To make its signature conform to  the standard `path.resolve`, avoid
    confusions.
    
    There's already a bug in the downstream plugin that was caused by such
    confusion: https://github.com/Akryum/vue-cli-plugin-ssr/blob/a742953b8376817950a01ad79967b9aa02da5388/generator/index.js#L163-L171
    
    * docs: update descriptions of the `resolve` api [ci skip]

--- packages/@vue/cli/lib/GeneratorAPI.js
+++ packages/@vue/cli/lib/GeneratorAPI.js
@@ -68,11 +68,11 @@ class GeneratorAPI {
   /**
    * Resolve path for a project.
    *
-   * @param {string} _path - Relative path from project root
-   * @return {string} The resolved absolute path.
+   * @param {string} _paths - A sequence of relative paths or path segments
+   * @return {string} The resolved absolute path, caculated based on the current project root.
    */
-  resolve (_path) {
-    return path.resolve(this.generator.context, _path)
+  resolve (..._paths) {
+    return path.resolve(this.generator.context, ..._paths)
   }
 
   get cliVersion () {
