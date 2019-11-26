b7f83b45ce4e6edeee0582f015a2ae73f86fc8e5
Haoqun Jiang <haoqunjiang@gmail.com>
2019-11-26 15:52:50 +0800

    fix: support `parser` option for codemods, and enable ts parsing by default (#4883)
    fixes #4861

--- packages/@vue/cli/lib/util/runCodemod.js
+++ packages/@vue/cli/lib/util/runCodemod.js
@@ -1,6 +1,30 @@
-const jscodeshift = require('jscodeshift')
 const adapt = require('vue-jscodeshift-adapter')
+let jscodeshift = require('jscodeshift')
 
-module.exports = function runCodemod (transform, fileInfo, options) {
-  return adapt(transform)(fileInfo, { jscodeshift }, options || {})
+module.exports = function runCodemod (transformModule, fileInfo, options = {}) {
+  const transform = typeof transformModule.default === 'function'
+    ? transformModule.default
+    : transformModule
+
+  let parser = transformModule.parser || options.parser
+  if (!parser) {
+    if (fileInfo.path.endsWith(('.ts'))) {
+      parser = 'ts'
+    } else if (fileInfo.path.endsWith('.tsx')) {
+      parser = 'tsx'
+    }
+  }
+
+  if (parser) {
+    jscodeshift = jscodeshift.withParser(parser)
+  }
+
+  const api = {
+    jscodeshift,
+    j: jscodeshift,
+    stats: () => {},
+    report: () => {}
+  }
+
+  return adapt(transform)(fileInfo, api, options)
 }
