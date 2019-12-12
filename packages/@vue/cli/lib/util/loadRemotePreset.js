f7b95acb9bc508e01d7d670645049008a7faa566
Haoqun Jiang <haoqunjiang@gmail.com>
2019-12-12 23:38:09 +0800

    fix: should download to different directories for different pre… (#4922)
    fixes #4778

--- packages/@vue/cli/lib/util/loadRemotePreset.js
+++ packages/@vue/cli/lib/util/loadRemotePreset.js
@@ -1,11 +1,20 @@
 const fs = require('fs-extra')
 const loadPresetFromDir = require('./loadPresetFromDir')
 
-module.exports = async function fetchRemotePreset (name, clone) {
+module.exports = async function loadRemotePreset (repository, clone) {
   const os = require('os')
   const path = require('path')
   const download = require('download-git-repo')
-  const tmpdir = path.join(os.tmpdir(), 'vue-cli')
+
+  const presetName = repository
+    .replace(/((?:.git)?#.*)/, '')
+    .split('/')
+    .slice(-1)[0]
+    // for direct urls, it's hard to get the correct project name,
+    // but we need to at least make sure no special characters remaining
+    .replace(/[:#]/g, '')
+
+  const tmpdir = path.join(os.tmpdir(), 'vue-cli-presets', presetName)
 
   // clone will fail if tmpdir already exists
   // https://github.com/flipxfx/download-git-repo/issues/41
@@ -14,7 +23,7 @@ module.exports = async function fetchRemotePreset (name, clone) {
   }
 
   await new Promise((resolve, reject) => {
-    download(name, tmpdir, { clone }, err => {
+    download(repository, tmpdir, { clone }, err => {
       if (err) return reject(err)
       resolve()
     })
