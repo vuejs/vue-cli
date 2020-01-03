1ba44241c6e61bd4e7df8d09622e30fde4dc62c3
Haoqun Jiang <haoqunjiang@gmail.com>
2020-01-03 21:46:51 +0800

    feat: don't throw on console/debugger statements for `vue serve`
    1. they're useful in development
    2. to be in line with the default preset

--- packages/@vue/cli-service-global/lib/globalConfigPlugin.js
+++ packages/@vue/cli-service-global/lib/globalConfigPlugin.js
@@ -112,6 +112,10 @@ module.exports = function createConfigPlugin (context, entry, asLib) {
                   ],
                   parserOptions: {
                     parser: 'babel-eslint'
+                  },
+                  rules: {
+                    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+                    'no-debugger': `process.env.NODE_ENV === 'production' ? 'error' : 'off'`
                   }
                 }
               }))
