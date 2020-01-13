71bcb0a9a8027c615614a35459eab6668d4a5c0d
Daniel Bächtold <dan@dstn.ch>
2020-01-13 14:50:05 +0100

    feat: adaptive icon support with maskable icons (#5053)

--- packages/@vue/cli-plugin-pwa/lib/HtmlPwaPlugin.js
+++ packages/@vue/cli-plugin-pwa/lib/HtmlPwaPlugin.js
@@ -23,6 +23,18 @@ const defaultManifest = {
       'src': './img/icons/android-chrome-512x512.png',
       'sizes': '512x512',
       'type': 'image/png'
+    },
+    {
+      'src': './img/icons/android-chrome-maskable-192x192.png',
+      'sizes': '192x192',
+      'type': 'image/png',
+      'purpose': 'maskable'
+    },
+    {
+      'src': './img/icons/android-chrome-maskable-512x512.png',
+      'sizes': '512x512',
+      'type': 'image/png',
+      'purpose': 'maskable'
     }
   ],
   start_url: '.',
