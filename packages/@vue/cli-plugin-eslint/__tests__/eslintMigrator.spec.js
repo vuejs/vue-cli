f4bea8471cd83471552ccf230a97b30f2102cfc4
Haoqun Jiang <haoqunjiang@gmail.com>
2020-01-27 20:03:02 +0800

    test: remove `.only`

--- packages/@vue/cli-plugin-eslint/__tests__/eslintMigrator.spec.js
+++ packages/@vue/cli-plugin-eslint/__tests__/eslintMigrator.spec.js
@@ -33,7 +33,7 @@ test('upgrade: should add eslint to devDependencies', async () => {
   expect(updatedPkg.devDependencies.eslint).toMatch('^4')
 })
 
-test.only('upgrade: should upgrade eslint from v5 to v6', async () => {
+test('upgrade: should upgrade eslint from v5 to v6', async () => {
   const project = await create('plugin-eslint-with-eslint-5', {
     plugins: {
       '@vue/cli-plugin-eslint': {
