76c173693e519babae9a5590ab1922fd19ad34f7
Haoqun Jiang <haoqunjiang@gmail.com>
2020-02-01 14:41:14 +0800

    fix: correctly catch errors of offline install

--- packages/@vue/cli/lib/util/ProjectPackageManager.js
+++ packages/@vue/cli/lib/util/ProjectPackageManager.js
@@ -243,9 +243,9 @@ class PackageManager {
   async install () {
     if (process.env.VUE_CLI_TEST) {
       try {
-        return this.runCommand([PACKAGE_MANAGER_CONFIG[this.bin].install, '--offline'])
+        await this.runCommand([PACKAGE_MANAGER_CONFIG[this.bin].install, '--offline'])
       } catch (e) {
-        this.runCommand([PACKAGE_MANAGER_CONFIG[this.bin].install])
+        await this.runCommand([PACKAGE_MANAGER_CONFIG[this.bin].install])
       }
     }
 
