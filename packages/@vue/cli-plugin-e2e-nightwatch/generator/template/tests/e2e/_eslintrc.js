1b64ff8464c317f8d5616626bf483f582c2e01c3
Haoqun Jiang <haoqunjiang@gmail.com>
2020-02-05 17:51:50 +0800

    fix: fix eslint errors for typescript + e2e-nightwatch setup

--- packages/@vue/cli-plugin-e2e-nightwatch/generator/template/tests/e2e/_eslintrc.js
+++ packages/@vue/cli-plugin-e2e-nightwatch/generator/template/tests/e2e/_eslintrc.js
@@ -1,6 +1,9 @@
 <%_ if (hasESLint) { _%>
 module.exports = {
   rules: {
+    <%_ if (hasESLint) { _%>
+    '@typescript-eslint/no-var-requires': 'off',
+    <%_ } _%>
     'no-unused-expressions': 'off'
   }
 }
