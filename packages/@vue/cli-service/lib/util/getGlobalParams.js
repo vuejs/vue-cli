a991de1fd9627350d51318a8835a9b73e803697d
Zyao89 <zyao89@gmail.com>
2020-03-12 12:51:38 +0800

    feat(cli-service): add "VUE_CLI_SERVICE_PARAMS"

--- packages/@vue/cli-service/lib/util/getGlobalParams.js
+++ packages/@vue/cli-service/lib/util/getGlobalParams.js
@@ -1,12 +1,9 @@
 const path = require('path')
-const isAbsoluteUrl = require('./isAbsoluteUrl')
 
 module.exports = function getGlobalParams (context) {
   let paramsPath = process.env.VUE_CLI_SERVICE_PARAMS
   if (paramsPath && typeof paramsPath === 'string') {
-    if (isAbsoluteUrl(paramsPath)) {
-      paramsPath = path.resolve(context, paramsPath)
-    }
+    paramsPath = path.resolve(context, paramsPath)
     try {
       let params = require(paramsPath)
       if (typeof params === 'function') {
