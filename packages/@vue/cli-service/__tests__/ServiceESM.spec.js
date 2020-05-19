767eba2d70353d3db2429ed927233f5e4b3b1d6a
Haoqun Jiang <haoqunjiang@gmail.com>
2020-05-19 16:38:22 +0800

    fix: should throw errors if there is bad require() in vue.config.js
    reverts #5305
    this makes the tests a little more tedious, need to find a better way
    to test these functionalities
    
    fixes #5442

--- packages/@vue/cli-service/__tests__/ServiceESM.spec.js
+++ packages/@vue/cli-service/__tests__/ServiceESM.spec.js
@@ -1,11 +1,27 @@
-const { join } = require('path')
 const Service = require('../lib/Service')
 
-const mockDir = join(__dirname, 'mockESM')
-const configPath = join(mockDir, 'vue.config.cjs')
+const configPath = '/vue.config.cjs'
+
+jest.mock('fs')
+const fs = require('fs')
+
+beforeEach(() => {
+  fs.writeFileSync('/package.json', JSON.stringify({
+    type: 'module',
+    vue: {
+      lintOnSave: 'default'
+    }
+  }, null, 2))
+})
+
+afterEach(() => {
+  if (fs.existsSync(configPath)) {
+    fs.unlinkSync(configPath)
+  }
+})
 
 const createService = () => {
-  const service = new Service(mockDir, {
+  const service = new Service('/', {
     plugins: [],
     useBuiltIn: false
   })
@@ -21,12 +37,14 @@ test('load project options from package.json', async () => {
 })
 
 test('load project options from vue.config.cjs', async () => {
+  fs.writeFileSync(configPath, '')
   jest.mock(configPath, () => ({ lintOnSave: true }), { virtual: true })
   const service = createService()
   expect(service.projectOptions.lintOnSave).toBe(true)
 })
 
 test('load project options from vue.config.cjs as a function', async () => {
+  fs.writeFileSync(configPath, '')
   jest.mock(configPath, () => function () { return { lintOnSave: true } }, { virtual: true })
   const service = createService()
   expect(service.projectOptions.lintOnSave).toBe(true)
