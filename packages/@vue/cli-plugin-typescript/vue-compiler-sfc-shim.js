bca23c6a711829297b31824e4f301702568a74ea
Haoqun Jiang <haoqunjiang@gmail.com>
2020-06-01 09:25:57 +0800

    fix: correctly shim @vue/compiler-sfc for fork-ts-checker-plugin
    Fixes the line padding to retain diagnostics location

--- packages/@vue/cli-plugin-typescript/vue-compiler-sfc-shim.js
+++ packages/@vue/cli-plugin-typescript/vue-compiler-sfc-shim.js
@@ -2,6 +2,13 @@ const compilerSFC = require('@vue/compiler-sfc')
 
 module.exports = {
   parseComponent (content, options) {
-    return compilerSFC.parse(content, options)
+    const result = compilerSFC.parse(content, options)
+    const { script } = result.descriptor
+
+    // fork-ts-checker-webpack-plugin needs to use the `start` property,
+    // which doesn't present in the `@vue/compiler-sfc` parse result
+    if (script) {
+      script.start = script.loc.start.offset
+    }
   }
 }
