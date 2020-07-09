c19dade78d185c70679f0f64f4c2eb5d40cb2309
Haoqun Jiang <haoqunjiang@gmail.com>
2020-07-09 11:54:21 +0800

    test: try to fix serve tests

--- packages/@vue/cli-service/__tests__/serve.spec.js
+++ packages/@vue/cli-service/__tests__/serve.spec.js
@@ -8,6 +8,10 @@ const serve = require('@vue/cli-test-utils/serveWithPuppeteer')
 
 const sleep = n => new Promise(resolve => setTimeout(resolve, n))
 
+afterEach(() => {
+  delete process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN
+})
+
 test('serve', async () => {
   const project = await create('e2e-serve', defaultPreset)
 
@@ -38,42 +42,6 @@ test('serve', async () => {
     }
   )
 })
-
-// TODO: need to fix vue package hoisting in Yarn workspace first
-test('serve with Vue 3', async () => {
-  process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN = true
-  const project = await create('e2e-serve-vue-3', Object.assign({}, defaultPreset, { vueVersion: '3' }))
-
-  await serve(
-    () => project.run('vue-cli-service serve'),
-    async ({ page, nextUpdate, helpers }) => {
-      const msg = `Welcome to Your Vue.js App`
-      expect(await helpers.getText('h1')).toMatch(msg)
-
-      // test hot reload
-      const file = await project.read(`src/App.vue`)
-      project.write(`src/App.vue`, file.replace(msg, `Updated`))
-      await nextUpdate() // wait for child stdout update signal
-      try {
-        await page.waitForFunction(selector => {
-          const el = document.querySelector(selector)
-          return el && el.textContent.includes('Updated')
-        }, { timeout: 60000 }, 'h1')
-      } catch (e) {
-        if (process.env.APPVEYOR && e.message.match('timeout')) {
-          // AppVeyor VM is so slow that there's a large chance this test cases will time out,
-          // we have to tolerate such failures.
-          console.error(e)
-        } else {
-          throw e
-        }
-      }
-    }
-  )
-
-  delete process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN
-})
-
 test('serve with router', async () => {
   const project = await create('e2e-serve-router', Object.assign({}, defaultPreset, {
     plugins: {
@@ -226,6 +194,38 @@ test('use a single websocket connection for HMR', async () => {
       expect(await helpers.getText('h1')).toMatch(msg)
 
       expect(requestUrls.filter(url => url.includes('sockjs-node')).length).toBe(1)
+    }
+  )
+})
+
+test('serve with Vue 3', async () => {
+  process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN = true
+  const project = await create('e2e-serve-vue-3', Object.assign({}, defaultPreset, { vueVersion: '3' }))
+
+  await serve(
+    () => project.run('vue-cli-service serve'),
+    async ({ page, nextUpdate, helpers }) => {
+      const msg = `Welcome to Your Vue.js App`
+      expect(await helpers.getText('h1')).toMatch(msg)
+
+      // test hot reload
+      const file = await project.read(`src/App.vue`)
+      project.write(`src/App.vue`, file.replace(msg, `Updated`))
+      await nextUpdate() // wait for child stdout update signal
+      try {
+        await page.waitForFunction(selector => {
+          const el = document.querySelector(selector)
+          return el && el.textContent.includes('Updated')
+        }, { timeout: 60000 }, 'h1')
+      } catch (e) {
+        if (process.env.APPVEYOR && e.message.match('timeout')) {
+          // AppVeyor VM is so slow that there's a large chance this test cases will time out,
+          // we have to tolerate such failures.
+          console.error(e)
+        } else {
+          throw e
+        }
+      }
     }
   )
 })
