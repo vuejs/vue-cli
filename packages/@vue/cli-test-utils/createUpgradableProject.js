69cad9cd3371fbfdbe699c3bfb3d6646f7dd94d5
Haoqun Jiang <haoqunjiang@gmail.com>
2020-07-09 10:39:18 +0800

    fix: no longer need an outside directory for upgrade tests

--- packages/@vue/cli-test-utils/createUpgradableProject.js
+++ packages/@vue/cli-test-utils/createUpgradableProject.js
@@ -1,18 +1,10 @@
-const fs = require('fs')
-const path = require('path')
-
 const createTestProject = require('./createTestProject')
 const Upgrader = require('@vue/cli/lib/Upgrader')
 
-const outsideTestFolder = path.resolve(__dirname, '../../../../vue-upgrade-tests')
-
 module.exports = async function createUpgradableProject (...args) {
-  if (!fs.existsSync(outsideTestFolder)) {
-    fs.mkdirSync(outsideTestFolder)
-  }
   process.env.VUE_CLI_TEST_DO_INSTALL_PLUGIN = true
 
-  const project = await createTestProject(...args, outsideTestFolder)
+  const project = await createTestProject(...args)
   const upgrade = async function upgrade (pluginName, options) {
     return (new Upgrader(project.dir)).upgrade(pluginName, options || {})
   }
