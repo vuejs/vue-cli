de37068d02c79c7935612f69d6984f28a021c5db
Haoqun Jiang <haoqunjiang@gmail.com>
2020-07-10 21:20:50 +0800

    fix: fix later-invoked plugin generators for Vue 3

--- packages/@vue/cli/lib/util/inferRootOptions.js
+++ packages/@vue/cli/lib/util/inferRootOptions.js
@@ -1,12 +1,25 @@
 // Infer rootOptions for individual generators being invoked
 // in an existing project.
-
+const { semver } = require('@vue/cli-shared-utils')
 module.exports = function inferRootOptions (pkg) {
   const rootOptions = {}
   const deps = Object.assign({}, pkg.dependencies, pkg.devDependencies)
 
   // projectName
   rootOptions.projectName = pkg.name
+
+  if ('vue' in deps) {
+    const vue2Range = semver.Range('^2.0.0')
+    const vue3Range = semver.Range('^3.0.0-0')
+
+    const depRange = semver.Range(deps.vue)
+
+    if (vue3Range.intersects(depRange)) {
+      rootOptions.vueVersion = '3'
+    } else if (vue2Range.intersects(depRange)) {
+      rootOptions.vueVersion = '2'
+    }
+  }
 
   // router
   if ('vue-router' in deps) {
