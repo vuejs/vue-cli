e5b17fac27772d00607bb3315057daed659bfe87
Haoqun Jiang <haoqunjiang@gmail.com>
2020-08-05 23:52:26 +0800

    fix: should not add to capabilities if corresponding browser is unchecked on creation

--- packages/@vue/cli-plugin-e2e-webdriverio/generator/template/wdio.local.conf.js
+++ packages/@vue/cli-plugin-e2e-webdriverio/generator/template/wdio.local.conf.js
@@ -9,17 +9,24 @@ exports.config = {
    * config for local testing
    */
   maxInstances: 1,
-  services: ['chromedriver', 'geckodriver'],
-  capabilities: [{
-    browserName: 'chrome',
-    acceptInsecureCerts: true,
-    'goog:chromeOptions': {
-      args: process.argv.includes('--headless')
-        ? ['--headless', '--disable-gpu']
-        : []
+  services: [<%- options.webdrivers.includes('chrome') ?(options.webdrivers.length > 1 ? `'chromedriver', ` : `'chromedriver'`) : '' %><%- options.webdrivers.includes('firefox') ? `'geckodriver'` : ''%>],
+  capabilities: [
+    <%_ if (options.webdrivers.includes('chrome')) { _%>
+    {
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        args: process.argv.includes('--headless')
+          ? ['--headless', '--disable-gpu']
+          : []
+      }
+    },
+    <%_ } _%>
+    <%_ if (options.webdrivers.includes('firefox')) { _%>
+    {
+      browserName: 'firefox',
+      acceptInsecureCerts: true
     }
-  }, {
-    browserName: 'firefox',
-    acceptInsecureCerts: true
-  }]
+    <%_ } _%>
+  ]
 }
