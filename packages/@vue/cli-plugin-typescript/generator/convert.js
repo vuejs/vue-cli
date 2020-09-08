bf4addd86186223f13dd0bb1f40018e0353fe2a1
Shinigami92 <chrissi92@hotmail.de>
2020-09-08 15:29:59 +0200

    chore: clean pr

--- packages/@vue/cli-plugin-typescript/generator/convert.js
+++ packages/@vue/cli-plugin-typescript/generator/convert.js
@@ -1,31 +1,30 @@
 module.exports = (api, { convertJsToTs = true } = {}) => {
-  const jsRE = /\.js$/;
-  let excludeRE = /^tests\/e2e\/|(\.config|rc)\.js$/;
+  const jsRE = /\.js$/
+  let excludeRE = /^tests\/e2e\/|(\.config|rc)\.js$/
 
-  if (api.hasPlugin("e2e-webdriverio")) {
-    excludeRE = /(\.config|rc)\.js$/;
+  if (api.hasPlugin('e2e-webdriverio')) {
+    excludeRE = /(\.config|rc)\.js$/
   }
-  const convertLintFlags = require("../lib/convertLintFlags");
   api.postProcessFiles((files) => {
     if (convertJsToTs) {
       // delete all js files that have a ts file of the same name
       // and simply rename other js files to ts
       for (const file in files) {
         if (jsRE.test(file) && !excludeRE.test(file)) {
-          const tsFile = file.replace(jsRE, ".ts");
+          const tsFile = file.replace(jsRE, '.ts')
           if (!files[tsFile]) {
-            const content = files[file];
-            files[tsFile] = content;
+            const content = files[file]
+            files[tsFile] = content
           }
-          delete files[file];
+          delete files[file]
         }
       }
     } else {
       // rename only main file to main.ts
-      const tsFile = api.entryFile.replace(jsRE, ".ts");
-      const content = files[api.entryFile];
-      files[tsFile] = content;
-      delete files[api.entryFile];
+      const tsFile = api.entryFile.replace(jsRE, '.ts')
+      const content = files[api.entryFile]
+      files[tsFile] = content
+      delete files[api.entryFile]
     }
-  });
-};
+  })
+}
--- packages/@vue/cli-plugin-typescript/generator/index.js
+++ packages/@vue/cli-plugin-typescript/generator/index.js
@@ -1,4 +1,4 @@
-const pluginDevDeps = require("../package.json").devDependencies;
+const pluginDevDeps = require('../package.json').devDependencies
 
 module.exports = (
   api,
@@ -6,107 +6,71 @@ module.exports = (
   rootOptions,
   invoking
 ) => {
-  if (typeof lintOn === "string") {
-    lintOn = lintOn.split(",");
+  if (typeof lintOn === 'string') {
+    lintOn = lintOn.split(',')
   }
-  const isVue3 = rootOptions && rootOptions.vueVersion === "3";
+  const isVue3 = rootOptions && rootOptions.vueVersion === '3'
 
   api.extendPackage({
     devDependencies: {
-      typescript: pluginDevDeps.typescript,
-    },
-  });
+      typescript: pluginDevDeps.typescript
+    }
+  })
 
   if (classComponent) {
     if (isVue3) {
       api.extendPackage({
         dependencies: {
-          "vue-class-component": "^8.0.0-0",
-        },
-      });
+          'vue-class-component': '^8.0.0-0'
+        }
+      })
     } else {
       api.extendPackage({
         dependencies: {
-          "vue-class-component": pluginDevDeps["vue-class-component"],
-          "vue-property-decorator": pluginDevDeps["vue-property-decorator"],
-        },
-      });
+          'vue-class-component': pluginDevDeps['vue-class-component'],
+          'vue-property-decorator': pluginDevDeps['vue-property-decorator']
+        }
+      })
     }
-  }
-
-  if (tsLint) {
-    api.extendPackage({
-      scripts: {
-        lint: "vue-cli-service lint",
-      },
-    });
-
-    if (!lintOn.includes("save")) {
-      api.extendPackage({
-        vue: {
-          lintOnSave: false,
-        },
-      });
-    }
-
-    if (lintOn.includes("commit")) {
-      api.extendPackage({
-        devDependencies: {
-          "lint-staged": "^9.5.0",
-        },
-        gitHooks: {
-          "pre-commit": "lint-staged",
-        },
-        "lint-staged": {
-          "*.ts": ["vue-cli-service lint", "git add"],
-          "*.vue": ["vue-cli-service lint", "git add"],
-        },
-      });
-    }
-
-    // lint and fix files on creation complete
-    api.onCreateComplete(() => {
-      return require("../lib/tslint")({}, api, true);
-    });
   }
 
   // late invoke compat
   if (invoking) {
-    if (api.hasPlugin("unit-mocha")) {
+    if (api.hasPlugin('unit-mocha')) {
       // eslint-disable-next-line node/no-extraneous-require
-      require("@vue/cli-plugin-unit-mocha/generator").applyTS(api);
+      require('@vue/cli-plugin-unit-mocha/generator').applyTS(api)
     }
 
-    if (api.hasPlugin("unit-jest")) {
+    if (api.hasPlugin('unit-jest')) {
       // eslint-disable-next-line node/no-extraneous-require
-      require("@vue/cli-plugin-unit-jest/generator").applyTS(api);
+      require('@vue/cli-plugin-unit-jest/generator').applyTS(api)
     }
 
-    if (api.hasPlugin("eslint")) {
+    if (api.hasPlugin('eslint')) {
       // eslint-disable-next-line node/no-extraneous-require
-      require("@vue/cli-plugin-eslint/generator").applyTS(api);
+      require('@vue/cli-plugin-eslint/generator').applyTS(api)
     }
 
-    if (api.hasPlugin("e2e-webdriverio")) {
+    if (api.hasPlugin('e2e-webdriverio')) {
       // eslint-disable-next-line node/no-extraneous-require
-      require("@vue/cli-plugin-e2e-webdriverio/generator").applyTS(api);
+      require('@vue/cli-plugin-e2e-webdriverio/generator').applyTS(api)
     }
   }
 
-  api.render("./template", {
+  api.render('./template', {
     skipLibCheck,
-    hasMocha: api.hasPlugin("unit-mocha"),
-    hasJest: api.hasPlugin("unit-jest"),
-    hasWebDriverIO: api.hasPlugin("e2e-webdriverio"),
-  });
+    hasMocha: api.hasPlugin('unit-mocha'),
+    hasJest: api.hasPlugin('unit-jest'),
+    hasWebDriverIO: api.hasPlugin('e2e-webdriverio')
+  })
 
   if (isVue3) {
-    api.render("./template-vue3");
+    api.render('./template-vue3')
 
     // In Vue 3, TSX interface is defined in https://github.com/vuejs/vue-next/blob/master/packages/runtime-dom/types/jsx.d.ts
     // So no need to manually add a shim.
-    api.render((files) => delete files["src/shims-tsx.d.ts"]);
+    api.render((files) => delete files['src/shims-tsx.d.ts'])
   }
 
-  require("./convert")(api, { tsLint, convertJsToTs });
-};
+  require('./convert')(api, { convertJsToTs })
+}
