4587386fc9ba2fd2d4973ad4004a7819697d96f4
Haoqun Jiang <haoqunjiang@gmail.com>
2020-09-10 21:06:27 +0800

    chore!: update eslint-loader, minimum supported ESLint version is 6
    closes #5699
    closes #5698

--- packages/@vue/cli-plugin-eslint/migrator/index.js
+++ packages/@vue/cli-plugin-eslint/migrator/index.js
@@ -1,4 +1,3 @@
-const inquirer = require('inquirer')
 const { semver } = require('@vue/cli-shared-utils')
 
 module.exports = async (api) => {
@@ -32,44 +31,35 @@ module.exports = async (api) => {
     return
   }
 
-  const { confirmUpgrade } = await inquirer.prompt([{
-    name: 'confirmUpgrade',
-    type: 'confirm',
-    message:
-    `Your current ESLint version is v${localESLintMajor}.\n` +
-    `The latest major version which supported by vue-cli is v6.\n` +
-    `Do you want to upgrade? (May contain breaking changes)\n`
-  }])
+  const { getDeps } = require('../eslintDeps')
 
-  if (confirmUpgrade) {
-    const { getDeps } = require('../eslintDeps')
-
-    const newDeps = getDeps(api)
-    if (pkg.devDependencies['@vue/eslint-config-airbnb']) {
-      Object.assign(newDeps, getDeps(api, 'airbnb'))
-    }
-    if (pkg.devDependencies['@vue/eslint-config-standard']) {
-      Object.assign(newDeps, getDeps(api, 'standard'))
-    }
-    if (pkg.devDependencies['@vue/eslint-config-prettier']) {
-      Object.assign(newDeps, getDeps(api, 'prettier'))
-    }
-
-    api.extendPackage({ devDependencies: newDeps }, { warnIncompatibleVersions: false })
-
-    // in case anyone's upgrading from the legacy `typescript-eslint-parser`
-    if (api.hasPlugin('typescript')) {
-      api.extendPackage({
-        eslintConfig: {
-          parserOptions: {
-            parser: '@typescript-eslint/parser'
-          }
-        }
-      })
-    }
-
-    // TODO:
-    // transform `@vue/prettier` to `eslint:recommended` + `@vue/prettier`
-    // transform `@vue/typescript` to `@vue/typescript/recommended` and also fix prettier compatibility for it
+  const newDeps = getDeps(api)
+  if (pkg.devDependencies['@vue/eslint-config-airbnb']) {
+    Object.assign(newDeps, getDeps(api, 'airbnb'))
   }
+  if (pkg.devDependencies['@vue/eslint-config-standard']) {
+    Object.assign(newDeps, getDeps(api, 'standard'))
+  }
+  if (pkg.devDependencies['@vue/eslint-config-prettier']) {
+    Object.assign(newDeps, getDeps(api, 'prettier'))
+  }
+
+  api.extendPackage({ devDependencies: newDeps }, { warnIncompatibleVersions: false })
+
+  // in case anyone's upgrading from the legacy `typescript-eslint-parser`
+  if (api.hasPlugin('typescript')) {
+    api.extendPackage({
+      eslintConfig: {
+        parserOptions: {
+          parser: '@typescript-eslint/parser'
+        }
+      }
+    })
+  }
+
+  api.exitLog(`ESLint upgraded from v${localESLintMajor}. to v6\n`)
+
+  // TODO:
+  // transform `@vue/prettier` to `eslint:recommended` + `@vue/prettier`
+  // transform `@vue/typescript` to `@vue/typescript/recommended` and also fix prettier compatibility for it
 }
