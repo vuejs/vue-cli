027386e8d15495ee6c2f738adb5f97139dc6c81e
Haoqun Jiang <haoqunjiang@gmail.com>
2020-09-30 16:34:40 +0800

    fix: update the `.vue` file shim for Vue 3 (#5903)

--- packages/@vue/cli-plugin-typescript/migrator/index.js
+++ packages/@vue/cli-plugin-typescript/migrator/index.js
@@ -1,4 +1,4 @@
-module.exports = api => {
+module.exports = (api, options, rootOptions) => {
   api.extendPackage(
     {
       devDependencies: {
@@ -7,4 +7,9 @@ module.exports = api => {
     },
     { warnIncompatibleVersions: false }
   )
+
+  // update vue 3 typescript shim
+  if (rootOptions.vueVersion === 3) {
+    api.transformScript('src/shims-vue.d.ts', require('../codemods/migrateComponentType'))
+  }
 }
