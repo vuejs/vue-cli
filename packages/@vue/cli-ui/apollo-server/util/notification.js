6b581bd978f390e1f74d2451a28d8c922b6723da
Tony Trinh <tony19@gmail.com>
2020-10-23 21:51:30 -0500

    feat: open browser when toast clicked

--- packages/@vue/cli-ui/apollo-server/util/notification.js
+++ packages/@vue/cli-ui/apollo-server/util/notification.js
@@ -6,10 +6,17 @@ const builtinIcons = {
   error: path.resolve(__dirname, '../../src/assets/error.png')
 }
 
+let notifCallback = null
+exports.setNotificationCallback = cb => {
+  notifCallback = cb
+    ? (_err, action) => (action === 'activate') && cb()
+    : null
+}
+
 exports.notify = ({ title, message, icon }) => {
   notifier.notify({
     title,
     message,
     icon: builtinIcons[icon] || icon
-  })
+  }, notifCallback)
 }
