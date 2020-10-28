689402c6c15fe67d9638bf21f81bfd24702b2330
jeneser <jeneserwang@gmail.com>
2020-10-28 17:07:53 +0000

    fix: update default vue 3 typescript shim

--- packages/@vue/cli-plugin-typescript/generator/template-vue3/src/shims-vue.d.ts
+++ packages/@vue/cli-plugin-typescript/generator/template-vue3/src/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<{}, {}, unknown>
   export default component
 }
