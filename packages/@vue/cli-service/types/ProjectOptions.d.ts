1e1e60acfc63ac40bf175808d140cab7c41e0dc7
Max Coplan <mchcopl@yahoo.com>
2020-11-02 08:45:54 -0500

    types(vue.config.js): Improve formatting in VSCode

--- packages/@vue/cli-service/types/ProjectOptions.d.ts
+++ packages/@vue/cli-service/types/ProjectOptions.d.ts
@@ -25,21 +25,25 @@ interface ExtractOptions {
 interface CSSOptions {
   /**
    * Default: `true`
+   *
    * By default, only files that ends in `*.module.[ext]` are treated as CSS modules
    */
   requireModuleExtension?: boolean;
   /**
    * Default: `true`
+   *
    * Whether to extract CSS in your components into a standalone CSS files (instead of inlined in JavaScript and injected dynamically)
    */
   extract?: boolean | ExtractOptions;
   /**
    * Default: `false`
+   *
    * Whether to enable source maps for CSS. Setting this to `true` may affect build performance
    */
   sourceMap?: boolean;
   /**
    * Default: `{}`
+   *
    * Pass options to CSS-related loaders
    */
   loaderOptions?: LoaderOptions;
@@ -48,46 +52,55 @@ interface CSSOptions {
 interface ProjectOptions {
   /**
    * Default: `'/'`
+   *
    * The base URL your application bundle will be deployed at
    */
   publicPath?: string;
   /**
    * Default: `'dist'`
+   *
    * The directory where the production build files will be generated in when running `vue-cli-service build`
    */
   outputDir?: string;
   /**
    * Default: `''`
+   *
    * A directory (relative to `outputDir`) to nest generated static assets (js, css, img, fonts) under
    */
   assetsDir?: string;
   /**
    * Default: `'index.html'`
+   *
    * Specify the output path for the generated `index.html` (relative to `outputDir`). Can also be an absolute path
    */
   indexPath?: string;
   /**
    * Default: `true`
+   *
    * By default, generated static assets contains hashes in their filenames for better caching control
    */
   filenameHashing?: boolean;
   /**
    * Default: `false`
+   *
    * Whether to use the build of Vue core that includes the runtime compiler
    */
   runtimeCompiler?: boolean;
   /**
    * Default: `[]`
+   *
    * If you want to explicitly transpile a dependency with Babel, you can list it in this option
    */
   transpileDependencies?: Array<string | RegExp>;
   /**
    * Default: `true`
+   *
    * Setting this to `false` can speed up production builds if you don't need source maps for production
    */
   productionSourceMap?: boolean;
   /**
    * Default: `require('os').cpus().length > 1`
+   *
    * Whether to use `thread-loader` for Babel or TypeScript transpilation
    */
   parallel?: boolean | number;
@@ -97,6 +110,7 @@ interface ProjectOptions {
   devServer?: { proxy: string | object, [key: string]: any };
   /**
    * Default: `undefined`
+   *
    * Build the app in multi-page mode
    */
   pages?: {
@@ -104,11 +118,13 @@ interface ProjectOptions {
   };
   /**
    * Default: `undefined`
+   *
    * Configure the `crossorigin` attribute on `<link rel="stylesheet">` and `<script>` tags in generated HTML
    */
   crossorigin?: '' | 'anonymous' | 'use-credentials';
   /**
    * Default: `false`
+   *
    * Set to `true` to enable [Subresource Integrity](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity) (SRI) on `<link rel="stylesheet">` and `<script>` tags in generated HTML
    */
   integrity?: boolean;
@@ -126,6 +142,7 @@ interface ProjectOptions {
 
   /**
    * Default: `'default'`
+   *
    * Whether to perform lint-on-save during development using [eslint-loader](https://github.com/webpack-contrib/eslint-loader)
    */
   lintOnSave?: boolean | 'default' | 'warning' | 'error';
