a9aa95cfecaf94778b7a852826a5970bf52ce6c4
jeneser <jeneserwang@gmail.com>
2020-11-13 10:43:17 +0000

    fix(cli-plugin-typescript): remove getPrompts function in prompts.js

--- packages/@vue/cli-plugin-typescript/prompts.js
+++ packages/@vue/cli-plugin-typescript/prompts.js
@@ -1,7 +1,7 @@
 // these prompts are used if the plugin is late-installed into an existing
 // project and invoked by `vue invoke`.
 
-const prompts = module.exports = [
+module.exports = [
   {
     name: `classComponent`,
     type: `confirm`,
@@ -32,11 +32,3 @@ const prompts = module.exports = [
     default: true
   }
 ]
-
-// in RC6+ the export can be function, but that would break invoke for RC5 and
-// below, so this is a temporary compatibility hack until we release stable.
-// TODO just export the function in 3.0.0
-module.exports.getPrompts = pkg => {
-  prompts[2].when = () => !('@vue/cli-plugin-eslint' in (pkg.devDependencies || {}))
-  return prompts
-}
