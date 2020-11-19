482ef1086bd78266f76488bcd4882145ac7e85c9
Haoqun Jiang <haoqunjiang@gmail.com>
2020-11-19 15:34:31 +0800

    chore!: bump joi to v17 (#5973)

--- packages/@vue/cli-shared-utils/lib/validate.js
+++ packages/@vue/cli-shared-utils/lib/validate.js
@@ -1,24 +1,33 @@
 const { exit } = require('./exit')
 
 // proxy to joi for option validation
-exports.createSchema = fn => fn(require('@hapi/joi'))
+exports.createSchema = fn => {
+  const joi = require('joi')
+
+  let schema = fn(joi)
+  if (typeof schema === 'object' && typeof schema.validate !== 'function') {
+    schema = joi.object(schema)
+  }
+
+  return schema
+}
 
 exports.validate = (obj, schema, cb) => {
-  require('@hapi/joi').validate(obj, schema, {}, err => {
-    if (err) {
-      cb(err.message)
-      if (process.env.VUE_CLI_TEST) {
-        throw err
-      } else {
-        exit(1)
-      }
+  const { error } = schema.validate(obj)
+  if (error) {
+    cb(error.details[0].message)
+
+    if (process.env.VUE_CLI_TEST) {
+      throw error
+    } else {
+      exit(1)
     }
-  })
+  }
 }
 
 exports.validateSync = (obj, schema) => {
-  const result = require('@hapi/joi').validate(obj, schema)
-  if (result.error) {
-    throw result.error
+  const { error } = schema.validate(obj)
+  if (error) {
+    throw error
   }
 }
