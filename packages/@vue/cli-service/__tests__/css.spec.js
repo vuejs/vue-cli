89b0377c4f60d292389cff4d70793b7780e7575e
Haoqun Jiang <haoqunjiang@gmail.com>
2020-12-08 13:21:37 +0800

    feat!: upgrade postcss-loader, using postcss 8 by default (#6108)

--- packages/@vue/cli-service/__tests__/css.spec.js
+++ packages/@vue/cli-service/__tests__/css.spec.js
@@ -62,7 +62,7 @@ test('default loaders', () => {
   LANGS.forEach(lang => {
     const loader = lang === 'css' ? [] : LOADERS[lang]
     expect(findLoaders(config, lang)).toEqual(['vue-style', 'css', 'postcss'].concat(loader))
-    expect(findOptions(config, lang, 'postcss').plugins).toEqual([require('autoprefixer')])
+    expect(findOptions(config, lang, 'postcss').postcssOptions.plugins).toEqual([require('autoprefixer')])
     // assert css-loader options
     expect(findOptions(config, lang, 'css')).toEqual({
       sourceMap: false,
@@ -83,7 +83,7 @@ test('production defaults', () => {
   LANGS.forEach(lang => {
     const loader = lang === 'css' ? [] : LOADERS[lang]
     expect(findLoaders(config, lang)).toEqual([extractLoaderPath, 'css', 'postcss'].concat(loader))
-    expect(findOptions(config, lang, 'postcss').plugins).toEqual([require('autoprefixer')])
+    expect(findOptions(config, lang, 'postcss').postcssOptions.plugins).toEqual([require('autoprefixer')])
     expect(findOptions(config, lang, 'css')).toEqual({
       sourceMap: false,
       importLoaders: 2
@@ -96,7 +96,7 @@ test('override postcss config', () => {
   LANGS.forEach(lang => {
     const loader = lang === 'css' ? [] : LOADERS[lang]
     expect(findLoaders(config, lang)).toEqual(['vue-style', 'css', 'postcss'].concat(loader))
-    expect(findOptions(config, lang, 'postcss').plugins).toBeFalsy()
+    expect(findOptions(config, lang, 'postcss').postcssOptions).toBeFalsy()
     // assert css-loader options
     expect(findOptions(config, lang, 'css')).toEqual({
       sourceMap: false,
@@ -254,7 +254,7 @@ test('css.extract', () => {
     // an additional instance of postcss-loader is injected for inline minification.
     expect(findLoaders(config, lang)).toEqual(['vue-style', 'css', 'postcss', 'postcss'].concat(loader))
     expect(findOptions(config, lang, 'css').importLoaders).toBe(3)
-    expect(findOptions(config, lang, 'postcss').plugins).toBeTruthy()
+    expect(findOptions(config, lang, 'postcss').postcssOptions.plugins).toBeTruthy()
   })
 
   const config2 = genConfig({
@@ -272,7 +272,7 @@ test('css.extract', () => {
     expect(findLoaders(config2, lang)).toEqual(['vue-style', 'css', 'postcss', 'postcss'].concat(loader))
     expect(findOptions(config2, lang, 'css').importLoaders).toBe(3)
     // minification loader should be injected before the user-facing postcss-loader
-    expect(findOptions(config2, lang, 'postcss').plugins).toBeTruthy()
+    expect(findOptions(config2, lang, 'postcss').postcssOptions.plugins).toBeTruthy()
   })
 })
 
---
+++ packages/@vue/cli-service/__tests__/cssPreprocessors.spec.js
@@ -0,0 +1,86 @@
+jest.setTimeout(30000)
+
+const create = require('@vue/cli-test-utils/createTestProject')
+const { defaultPreset } = require('@vue/cli/lib/options')
+
+test('autoprefixer', async () => {
+  const project = await create('css-autoprefixer', defaultPreset)
+
+  await project.write('vue.config.js', 'module.exports = { filenameHashing: false }\n')
+
+  const appVue = await project.read('src/App.vue')
+  await project.write('src/App.vue', appVue.replace('#app {', '#app {\n  user-select: none;'))
+
+  await project.run('vue-cli-service build')
+
+  const css = await project.read('dist/css/app.css')
+  expect(css).toMatch('-webkit-user-select')
+})
+
+test('CSS inline minification', async () => {
+  const project = await create('css-inline-minification', defaultPreset)
+
+  await project.write('vue.config.js', 'module.exports = { filenameHashing: false, css: { extract: false } }\n')
+
+  const appVue = await project.read('src/App.vue')
+  await project.write('src/App.vue',
+    appVue.replace(
+      '#app {',
+
+      '#app {\n  height: calc(100px * 2);'
+    )
+  )
+  await project.run('vue-cli-service build')
+  const appJs = await project.read('dist/js/app.js')
+  expect(appJs).not.toMatch('calc(100px')
+  expect(appJs).toMatch('height:200px;')
+})
+
+test('CSS minification', async () => {
+  const project = await create('css-minification', defaultPreset)
+
+  await project.write('vue.config.js', 'module.exports = { filenameHashing: false }\n')
+
+  const appVue = await project.read('src/App.vue')
+  await project.write('src/App.vue',
+    appVue.replace(
+      '#app {',
+
+      '#app {\n  height: calc(100px * 2);'
+    )
+  )
+  await project.run('vue-cli-service build')
+  const appCss = await project.read('dist/css/app.css')
+  expect(appCss).not.toMatch('calc(100px')
+  expect(appCss).toMatch('height:200px;')
+})
+
+test('Custom PostCSS plugins', async () => {
+  const project = await create('css-custom-postcss', defaultPreset)
+  await project.write('vue.config.js', `
+  const toRedPlugin = () => {
+    return {
+      postcssPlugin: 'to-red',
+      Declaration (decl) {
+        if (decl.prop === 'color') {
+          decl.value = 'red'
+        }
+      }
+    }
+  }
+  toRedPlugin.postcss = true
+
+  module.exports = {
+    filenameHashing: false,
+    css: {
+      loaderOptions: {
+        postcss: {
+          postcssOptions: { plugins: [toRedPlugin] }
+        }
+      }
+    }
+  }`)
+  await project.run('vue-cli-service build')
+  const appCss = await project.read('dist/css/app.css')
+  expect(appCss).toMatch('color:red')
+})
