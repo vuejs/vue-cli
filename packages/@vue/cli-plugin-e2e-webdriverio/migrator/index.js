bae9a9cf5451dc2103d22135f4cc6fe55dffb445
Haoqun Jiang <haoqunjiang@gmail.com>
2021-02-23 15:21:13 +0800

    fix(webdriverio): add `expect-webdriverio` to tsconfig

--- packages/@vue/cli-plugin-e2e-webdriverio/migrator/index.js
+++ packages/@vue/cli-plugin-e2e-webdriverio/migrator/index.js
@@ -1,11 +1,20 @@
 module.exports = (api) => {
   if (api.fromVersion('<= 5.0.0-alpha.4')) {
-    api.render(files => {
+    api.render((files) => {
       if (!files['tsconfig.json']) {
         return
       }
 
-      files['tsconfig.json'] = files['tsconfig.json'].replace('"@wdio/sync"', '"webdriverio/sync"')
+      files['tsconfig.json'] = files['tsconfig.json'].replace(
+        '"@wdio/sync"',
+        '"webdriverio/sync"'
+      )
+      if (!/"expect-webdriverio"/.test(files['tsconfig.json'])) {
+        files['tsconfig.json'] = files['tsconfig.json'].replace(
+          '"@wdio/mocha-framework",',
+          '"@wdio/mocha-framework",\n      "expect-webdriverio",'
+        )
+      }
     })
   }
 }
