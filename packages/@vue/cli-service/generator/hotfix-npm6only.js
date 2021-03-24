b508781ea5e1fa81c0e6e41214184cd8e8bdbc57
Haoqun Jiang <haoqunjiang@gmail.com>
2021-03-24 16:09:02 +0800

    Revert "fix: work around npm6/postcss8 hoisting issue (#6358)"
    This reverts commit 27b4263ed299e219aa5fbe7b024c226e5775c549.

--- packages/@vue/cli-service/generator/hotfix-npm6only.js
+++
@@ -1,8 +0,0 @@
-// this file will be renamed to hotfix.js if the package is installed by npm 6
-module.exports = (api) => {
-  api.extendPackage({
-    dependencies: {
-      postcss: '^8.2.6'
-    }
-  })
-}
