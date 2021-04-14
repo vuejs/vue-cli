bf4e0a7b695560084dbab8eb45ca78e94f5ec942
Maksim Nedoshev <m0ksem1337@gmail.com>
2021-04-14 11:25:47 +0300

    Resolve ts-jest module in typescript jest-preset.
    Co-authored-by: Haoqun Jiang <haoqunjiang@gmail.com>

--- packages/@vue/cli-plugin-unit-jest/presets/typescript/jest-preset.js
+++ packages/@vue/cli-plugin-unit-jest/presets/typescript/jest-preset.js
@@ -3,7 +3,7 @@ const defaultPreset = require('../default/jest-preset')
 
 let tsJest = null
 try {
-  tsJest = require('ts-jest')
+  tsJest = require.resolve('ts-jest')
 } catch (e) {
   throw new Error('Cannot resolve "ts-jest" module. Typescript preset requires "ts-jest" to be installed.')
 }
