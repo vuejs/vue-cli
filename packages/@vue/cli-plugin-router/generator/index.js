398a8bc05cb6704b7a13b116fd1f5b8ca3ee2910
杨少峰 <yangshaofengfe@gmail.com>
2021-04-19 18:06:42 +0800

    code-style-update

--- packages/@vue/cli-plugin-router/generator/index.js
+++ packages/@vue/cli-plugin-router/generator/index.js
@@ -20,19 +20,11 @@ module.exports = (api, options = {}, rootOptions = {}) => {
     })
   }
 
-  api.render('./template', {
+  api.render(`./template${isVue3 && '-vue3'}`, {
     historyMode: options.historyMode,
     doesCompile: api.hasPlugin('babel') || api.hasPlugin('typescript'),
     hasTypeScript: api.hasPlugin('typescript')
   })
-
-  if (isVue3) {
-    api.render('./template-vue3', {
-      historyMode: options.historyMode,
-      doesCompile: api.hasPlugin('babel') || api.hasPlugin('typescript'),
-      hasTypeScript: api.hasPlugin('typescript')
-    })
-  }
 
   if (api.invoking) {
     if (api.hasPlugin('typescript')) {
