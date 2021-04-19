cc35736bc805e79ffe4d59db8b6337efc69eecbe
杨少峰 <yangshaofengfe@gmail.com>
2021-04-19 18:42:35 +0800

    eslint fix

--- packages/@vue/cli-plugin-router/generator/index.js
+++ packages/@vue/cli-plugin-router/generator/index.js
@@ -19,7 +19,7 @@ module.exports = (api, options = {}, rootOptions = {}) => {
       }
     })
   }
-  
+
   api.render(`./template${isVue3 ? '-vue3' : ''}`, {
     historyMode: options.historyMode,
     doesCompile: api.hasPlugin('babel') || api.hasPlugin('typescript'),
