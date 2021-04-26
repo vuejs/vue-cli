d12cd5ef5de7922bf017dc6a4952f89373bc2ea4
Binwei Fang <fangbinwei@yeah.net>
2021-04-27 02:47:46 +0800

    fix(mocha): workaround the dart-sass stylesheet load issue

--- packages/@vue/cli-plugin-unit-mocha/setup.js
+++ packages/@vue/cli-plugin-unit-mocha/setup.js
@@ -5,4 +5,9 @@ window.Date = Date
 // https://github.com/vuejs/vue-next/pull/2943
 global.ShadowRoot = window.ShadowRoot
 
+// https://github.com/vuejs/vue-next/issues/3590
 global.SVGElement = window.SVGElement
+
+// https://github.com/vuejs/vue-cli/issues/6427
+// https://github.com/mbullington/node_preamble.dart/issues/26
+process.versions.electron = ''
