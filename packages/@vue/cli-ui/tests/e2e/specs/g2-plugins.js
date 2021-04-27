b2fcaaee32dfd3a755d08be2cf4757ba9b0e2923
Haoqun Jiang <haoqunjiang@gmail.com>
2021-04-27 19:19:58 +0800

    test: bump timeout for skip button

--- packages/@vue/cli-ui/tests/e2e/specs/g2-plugins.js
+++ packages/@vue/cli-ui/tests/e2e/specs/g2-plugins.js
@@ -25,7 +25,7 @@ describe('Plugins', () => {
     cy.get('[data-testid="finish-install"]').should('not.have.class', 'disabled').click({ force: true })
     cy.get('.loading-screen .vue-ui-loading-indicator', { timeout: 3000 }).should('be.visible')
     cy.get('.file-diff-view', { timeout: 250000 }).should('be.visible')
-    cy.get('[data-testid="skip-button"]', { timeout: 3000 })
+    cy.get('[data-testid="skip-button"]', { timeout: 30000 })
       .should('be.visible')
       .should('not.have.class', 'disabled')
       .click()
