6c4db2598c9785b0d7838da907290647255eaef3
Haoqun Jiang <haoqunjiang@gmail.com>
2021-05-07 14:44:56 +0800

    fix: fix jest migrator dependency merging (#6454)
    Thanks to @PiDelport

--- packages/@vue/cli-plugin-unit-jest/migrator/index.js
+++ packages/@vue/cli-plugin-unit-jest/migrator/index.js
@@ -1,7 +1,7 @@
 /** @param {import('@vue/cli/lib/MigratorAPI')} api MigratorAPI */
 module.exports = (api) => {
   api.extendPackage(pkg => {
-    const toMerge = {}
+    const newDevDeps = {}
 
     const allDeps = {
       ...pkg.dependencies,
@@ -12,13 +12,14 @@ module.exports = (api) => {
     if (!allDeps['vue-jest']) {
       // Likely a Vue 2 project, and uses the builtin preset.
       // Because we used to add `vue-jest` v5 to dev deps for Vue 3 projects.
-      toMerge['vue-jest'] = '^4.0.1'
+      newDevDeps['vue-jest'] = '^4.0.1'
     }
 
     if (allDeps['@vue/cli-plugin-typescript'] && !allDeps['ts-jest']) {
-      toMerge['ts-jest'] = '^26.5.3'
+      newDevDeps['ts-jest'] = '^26.5.3'
     }
 
+    const toMerge = { devDependencies: newDevDeps }
     return toMerge
   })
 }
