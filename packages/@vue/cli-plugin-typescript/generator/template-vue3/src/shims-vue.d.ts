27854ba1eaeca34c2ccb130bb9f95947c2ddd954
Amour1688 <lcz_1996@foxmail.com>
2021-05-08 13:51:37 +0800

    feat(types): jsx directive types

--- packages/@vue/cli-plugin-typescript/generator/template-vue3/src/shims-vue.d.ts
+++ packages/@vue/cli-plugin-typescript/generator/template-vue3/src/shims-vue.d.ts
@@ -4,3 +4,18 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+type OneOf<T extends string[]> = T[number]
+
+type Directive<
+  Exp,
+  Arg extends string | undefined = undefined,
+  Modifiers extends string[] = never[]
+  > = [Exp, Arg, OneOf<Modifiers>[]] | [Exp, OneOf<Modifiers>[]] | Exp | [Exp] | [Exp, Arg]
+
+
+declare namespace JSX {
+  interface IntrinsicAttributes {
+    'v-show': Directive<boolean>;
+  }
+}
