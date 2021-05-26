626cdb50e64aea023fe5de00030dc1a5a1e79678
Karen Baney <karen_baney@shamrockfoods.com>
2021-05-26 13:09:05 -0700

    remove commented code

--- packages/@vue/cli-plugin-workbox-pwa/prompts.js
+++ packages/@vue/cli-plugin-workbox-pwa/prompts.js
@@ -16,16 +16,4 @@ module.exports = [
     ],
     default: 'InjectManifest'
   }
-  // {
-  //   name: 'addManualUpdate',
-  //   type: 'confirm',
-  //   message: 'Add code for prompt to user for manual updates of the service worker (y/N)?',
-  //   default: false
-  // },
-  // {
-  //   name: 'addAutoUpdate',
-  //   type: 'confirm',
-  //   message: 'Add code to auto update the service worker at an interval (default 1 hr) (y/N)?',
-  //   default: false
-  // }
 ]
