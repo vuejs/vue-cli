0a3983a8a6fb19a1135abe1cbe2f7fb1e6433a15
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-04 17:38:41 +0800

    test: list things we need to validate before shipping the cache feature

---
+++ packages/@vue/cli-service/__tests__/cache.spec.js
@@ -0,0 +1,21 @@
+test.todo('should ignore cache when `--no-cache` argument is passed')
+
+test.todo('should ignore previous cache when `--mode` argument changed')
+
+test.todo('should ignore previous cache when corresponding `.env` file changed')
+
+test.todo('should not ignore previous cache when irrelevant `.env` file changed')
+
+test.todo('should ignore previous cache when `--target` argument changed')
+
+test.todo('should ignore previous cache when `--module` flag is changed')
+
+test.todo('should ignore previous cache when `package.json` changed')
+
+test.todo('should ignore previous cache when lockfiles changed')
+
+test.todo('should ignore previous cache when Vue CLI config file changed')
+
+test.todo('should ignore previous cache when babel config changed')
+
+test.todo('should ignore previous cache when tsconfig.json changed')
