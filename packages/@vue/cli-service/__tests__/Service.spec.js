4c753bc7e8b42c2c64bc0608cfbdff2ff5d87315
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-09 13:15:07 +0800

    chore: pre release sync

--- packages/@vue/cli-service/__tests__/Service.spec.js
+++ packages/@vue/cli-service/__tests__/Service.spec.js
@@ -68,7 +68,7 @@ test('loading plugins from package.json', () => {
   mockPkg({
     devDependencies: {
       bar: '^1.0.0',
-      '@vue/cli-plugin-babel': '^5.0.0-beta.1',
+      '@vue/cli-plugin-babel': '^5.0.0-beta.2',
       'vue-cli-plugin-foo': '^1.0.0'
     }
   })
