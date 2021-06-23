882aaca1fc80f7f478bac2f309b5e9b7f1acfe6b
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-23 15:32:36 +0800

    feat: add support for avif

--- packages/@vue/cli-plugin-unit-jest/presets/default/jest-preset.js
+++ packages/@vue/cli-plugin-unit-jest/presets/default/jest-preset.js
@@ -16,7 +16,7 @@ module.exports = {
   transform: {
     // process *.vue files with vue-jest
     '^.+\\.vue$': vueJest,
-    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
     require.resolve('jest-transform-stub'),
     '^.+\\.jsx?$': require.resolve('babel-jest')
   },
