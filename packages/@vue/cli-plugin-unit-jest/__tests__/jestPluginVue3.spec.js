d37135d0b6fd0d1068505c9a0b4091ece4024554
cexbrayat <cedric@ninja-squad.com>
2021-08-08 10:42:26 +0200

    feat: update jest to v27
    Fixes #6602
    
    This commit updates the generated project to use:
    - jest v27, as an explicit dependency
    - ts-jest and babel-jest v27
    - @vue/vue3-jest as a transformer for Vue 3 projects
    - @vue/vue2-jest as a transformer for Vue 2 projects
    
    The default Babel transformer has been updated to work with Jest v27 as well.

--- packages/@vue/cli-plugin-unit-jest/__tests__/jestPluginVue3.spec.js
+++ packages/@vue/cli-plugin-unit-jest/__tests__/jestPluginVue3.spec.js
@@ -12,5 +12,24 @@ test('should work with Vue 3', async () => {
   })
   const pkg = JSON.parse(await project.read('package.json'))
   expect(pkg.devDependencies['@vue/test-utils']).toMatch('^2')
+  expect(pkg.devDependencies['@vue/vue3-jest']).toMatch('^27')
+  expect(pkg.devDependencies.jest).toMatch('^27')
+  expect(pkg.devDependencies['babel-jest']).toMatch('^27')
+  await project.run(`vue-cli-service test:unit`)
+})
+
+test('should work with Vue 3 and TS', async () => {
+  const project = await createOutside('unit-jest-vue-3', {
+    vueVersion: '3',
+    plugins: {
+      '@vue/cli-plugin-typescript': {},
+      '@vue/cli-plugin-unit-jest': {}
+    }
+  })
+  const pkg = JSON.parse(await project.read('package.json'))
+  expect(pkg.devDependencies['@vue/test-utils']).toMatch('^2')
+  expect(pkg.devDependencies['@vue/vue3-jest']).toMatch('^27')
+  expect(pkg.devDependencies.jest).toMatch('^27')
+  expect(pkg.devDependencies['ts-jest']).toMatch('^27')
   await project.run(`vue-cli-service test:unit`)
 })
