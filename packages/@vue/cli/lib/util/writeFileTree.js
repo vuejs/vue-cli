1ff6e1930bb45ceb3120501c7274c2023d1d9bff
ChrisMiaoMiao <xiong_zixiao@163.com>
2021-09-13 17:30:23 +0800

    fix(cli): do not remove empty file when use vue add command

--- packages/@vue/cli/lib/util/writeFileTree.js
+++ packages/@vue/cli/lib/util/writeFileTree.js
@@ -4,8 +4,8 @@ const path = require('path')
 function deleteRemovedFiles (directory, newFiles, previousFiles) {
   // get all files that are not in the new filesystem and are still existing
   const filesToDelete = Object.keys(previousFiles)
-    .filter(filename => !newFiles[filename])
-
+    .filter(filename => !newFiles[filename] && newFiles[filename] !== '')
+  
   // delete each of these files
   return Promise.all(filesToDelete.map(filename => {
     return fs.unlink(path.join(directory, filename))
