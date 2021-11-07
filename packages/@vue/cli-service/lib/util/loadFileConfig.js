ad9a0feedce0fa32f8fbd29520e5ca7696610b27
screetBloom <15306225869@163.com>
2021-11-08 01:45:24 +0800

    feat: support vue.config.ts

--- packages/@vue/cli-service/lib/util/loadFileConfig.js
+++ packages/@vue/cli-service/lib/util/loadFileConfig.js
@@ -1,34 +1,33 @@
-const fs = require('fs')
-const path = require('path')
+// @ts-check
+const { existsSync } = require('fs')
+const { join } = require('path')
+const { register } = require('esbuild-register/dist/node')
 
-const isFileEsm = require('is-file-esm')
-const { loadModule } = require('@vue/cli-shared-utils')
+const POSSIBLE_CONFIG_PATHS = [
+  process.env.VUE_CLI_SERVICE_CONFIG_PATH,
+  'vue.config.js',
+  'vue.config.cjs',
+  'vue.config.mjs',
+  'vue.config.ts'
+].filter(i => !!i)
 
 module.exports = function loadFileConfig (context) {
-  let fileConfig, fileConfigPath
+  let fileConfig
 
-  const possibleConfigPaths = [
-    process.env.VUE_CLI_SERVICE_CONFIG_PATH,
-    './vue.config.js',
-    './vue.config.cjs',
-    './vue.config.mjs'
-  ]
-  for (const p of possibleConfigPaths) {
-    const resolvedPath = p && path.resolve(context, p)
-    if (resolvedPath && fs.existsSync(resolvedPath)) {
-      fileConfigPath = resolvedPath
-      break
-    }
-  }
+  const formatPath = p => join(context, p)
+  const fileConfigPath = POSSIBLE_CONFIG_PATHS.map(formatPath).find(p => existsSync(p))
+  const compatESModuleRequire = m => m.__esModule ? m.default : m
 
   if (fileConfigPath) {
-    const { esm } = isFileEsm.sync(fileConfigPath)
-
-    if (esm) {
-      fileConfig = import(fileConfigPath)
-    } else {
-      fileConfig = loadModule(fileConfigPath, context)
-    }
+    // Use esbuild to compile conifg files
+    register({
+      target: 'es2017',
+      format: 'cjs'
+    })
+    const module = require('module')
+    // @ts-ignore
+    console.log(module.Module._extensions)
+    fileConfig = compatESModuleRequire(require(fileConfigPath))
   }
 
   return {
