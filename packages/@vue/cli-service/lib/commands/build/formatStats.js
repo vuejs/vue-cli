50a923af33a8c8e50659f1e544b7127948968e1b
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-02-02 20:22:24 +0800

    feat: add build stats hash support

--- packages/@vue/cli-service/lib/commands/build/formatStats.js
+++ packages/@vue/cli-service/lib/commands/build/formatStats.js
@@ -68,5 +68,10 @@ module.exports = function formatStats (stats, dir, api) {
     )).join(`\n`)
   )
 
-  return `${ui.toString()}\n\n  ${chalk.gray(`Images and other types of assets omitted.`)}\n`
+  const time = stats.endTime - stats.startTime
+  const now = new Date().toISOString()
+  const hash = stats.hash
+  const info = `Build at: ${chalk.white(now)} - Hash: ${chalk.white(hash)} - Time: ${chalk.white(time)}ms`
+
+  return `${ui.toString()}\n\n  ${chalk.gray(`Images and other types of assets omitted.`)}\n  ${info}\n`
 }
