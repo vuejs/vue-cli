c21c15665538b5117d21354ae52b1939895b5e68
Bob van de Vijver <bobvandevijver@users.noreply.github.com>
2025-08-21 11:09:14 +0200

    fix: add missing default `__VUE_PROD_HYDRATION_MISMATCH_DETAILS__` (#7443)
    Fixes vue v3.5.19 compatibility

--- packages/@vue/cli-service/lib/config/base.js
+++ packages/@vue/cli-service/lib/config/base.js
@@ -158,7 +158,8 @@ module.exports = (api, options) => {
         .plugin('feature-flags')
           .use(webpack.DefinePlugin, [{
             __VUE_OPTIONS_API__: 'true',
-            __VUE_PROD_DEVTOOLS__: 'false'
+            __VUE_PROD_DEVTOOLS__: 'false',
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
           }])
     }
 
